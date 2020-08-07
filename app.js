457b8e11870599b8751a970e4ec17849ec21d124
Stacy Sealky <stacy.sealky.lee@gmail.com>
2020-08-07 18:23:24 -0500

    routes fixed, views connected

--- app.js
+++ app.js
@@ -9,7 +9,6 @@
  * (add more comments later)
  */
 
-
 //EXPRESS SET UP 
 const express = require('express');
 const app = express();
@@ -25,20 +24,24 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const xss = require('xss');
 const dotenv = require('dotenv');
-// const webToken = require('jsonwebtoken');
+const webToken = require('jsonwebtoken');
 const ejs = require('ejs');
-app.set('view engine', 'ejs');
+
+//SET VIEW ENGINE AND RENDER HTML WITH EJS
+app.set("view engine", "ejs");
+app.engine('html', ejs.renderFile);
 
 //DEV ENV
 const hostname = '127.0.0.1';
 const port = 8081;
-
-// LIVE PRODUCTION ENV
+// LIVE ENV
 // const hostname = '157.245.243.18';
 // const port = 80;
 
-//LOAD ENV VARS
-dotenv.config({ path: './config/config.env' });
+//LOAD CONFIG.ENV VARS
+dotenv.config({
+	path: './config/config.env'
+});
 
 //PARSERS SET UP
 app.use(bodyParser.json());
@@ -48,25 +51,24 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 
+//SESSION SET UP
 app.use(session({
 	store: new MongoStore({
 		url: process.env.MONGO_URI,
 		clear_interval: 3600000
 	}),
-	name: process.env.SESS_NAME, 
+	name: process.env.SESS_NAME,
 	resave: false,
 	saveUninitialized: false,
 	secret: process.env.SESS_SECRET,
 	cookie: {
-		maxAge: Number(process.env.SESS_LIFE),	// cookie set to expire in 1 hour 
+		maxAge: Number(process.env.SESS_LIFE), // cookie set to expire in 1 hour 
 		sameSite: true,
 		secure: process.env.NODE_ENV === 'production'
 	}
 }));
 
-
 //DB SET UP & APP LISTEN (server starts after db connection)
-//DB URI IN CONFIG.ENV - DO NOT PUSH CONFIG.ENV
 var options = {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
@@ -83,42 +85,39 @@ mongoose.connect(process.env.MONGO_URI, options, (err, database) => {
 	}
 });
 
-//ROUTE FILES - **ADD MORE HERE
-// app.use(express.static('public'));
-// app.use(express.static('assets'));
+//STATIC SET UP
+app.use(express.static('public'));
+app.use(express.static('assets'));
+app.use(express.static(__dirname));
+
+//IMPORT ROUTE FILES
 const usersRoute = require('./routes/users');
+
+//MOUNT ROUTERS
+app.use('/users', usersRoute);
 
 
 //IMPORT MODELS
 const User = require('./models/User');
 const Contact = require('./models/Contact');
 
+
 // middleware for extracting userId from a session
-app.use('/users', async (req, res, next) => {
-	const { userId } = req.session;
-	if (userId) {
-		const result = await User.findById(userId);
-		res.locals.user = result; 
-	}
-	next();
-});
+// app.use('/users', async (req, res, next) => {
+// 	const {
+// 		userId
+// 	} = req.session;
+// 	if (userId) {
+// 		const result = await User.findById(userId);
+// 		res.locals.user = result;
+// 	}
+// 	next();
+// });
 
-//TODO: MOUNT ROUTERS HERE
-app.use('/users', usersRoute);
-
-//UNHANDLED REJECTION - PROMISE
-process.on('unhandledRejection', (err, promise) => {
-	console.log(`Error: ${err.message}`);
-	app.close(() => process.exit(1)); // what is this trying to do? there is no app.close function 
-})
-
-// PREVENT XSS ATTACKS
-// app.use(xss());
 
 // BASIC HOME ROUTING 
-// ***(remove or edit this later after completing routes and controllers)
-app.get('/', (req,res) => {
-	res.sendFile(path.join(__dirname,'public','index.html'));
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: __dirname });
 });
 
 //EMAIL SENDING FUNCTION LIVES IN THE BELOW CONTROLLER
--- routes/users.js
+++ routes/users.js
@@ -1,65 +1,211 @@
-//TODO: authorize 'admin'
+/*
+    CHANGES MADE: we will just use /routes and mount on app.js
+    I moved the controllers and merged them into one file here.
+*/
 
+//NPM DEPENDENCIES
 const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const path = require('path');
+const mongoose = require('mongoose');
 
-const redirectLogin = (req, res, next) => {
-	if (!req.session.userId) {
-		res.redirect('/login');
-	} else {
-		next();
-	}
-}
-
-const redirectProfile = (req, res, next) => {
-	if (req.session.userId) {
-		res.redirect('/profile');
-	} else {
-		next();
-	}
-}
-
-const {
-    getUsersRoot,
-    getUsersProfile,
-    getUsersLogin,
-    getUsersRegister,
-    postUsersLogin, 
-    postUsersRegister, 
-    postUsersLogout
-} = require('../controllers/users');
-
+//IMPORT USER MODEL
 const User = require('../models/User');
 
-const router = express.Router({
-    mergeParams: true
+//LOAD STATIC VIEWS
+router.use(express.static('public'));
+router.use(express.static('assets'));
+
+
+// @desc    Render index.html (home)
+// @route   GET '/users'
+// @access  Public
+// @tested 	Yes
+router.get('/', (req, res) => {
+    try {
+        res.status(200).sendFile( path.join( __dirname, '../public', 'index.html' )); 
+    } catch (error) {
+        res.status(400).send(JSON.stringify({
+			success: false,
+			error: err
+		}));
+    }
 });
 
-router
-    .route('/')
-    .get(getUsersRoot);
+// @desc    Render signup.html
+// @route   GET '/users/signup'
+// @access  Public
+// @tested 	Yes
+router.get('/signup', (req, res) => {
+    try {
+        res.status(200).sendFile( path.join( __dirname, '../public', 'signup.html' )); 
+    } catch (error) {
+        res.status(400).send(JSON.stringify({
+			success: false,
+			error: err
+		}));
+    }
+});
 
-router
-    .route('/profile')
-    .get(redirectLogin, getUsersProfile);
+// @desc    
+// @route   GET '/users/login'
+// @access  Private
+// @tested 	Not yet
+router.get('/login', (req, res) => {
+    try {
+        res.status(200).sendFile( path.join( __dirname, '../public', 'login.html' ));
+    } catch (error) {
+        res.status(400).send(JSON.stringify({
+			success: false,
+			error: err
+		}));
+    }
+});
 
-router
-    .route('/login')
-    .get(redirectProfile, getUsersLogin);
+// @desc    Render profile.html, grabs userId and render ejs data in static template
+// @route   GET '/users/profile/:id'
+// @access  Private, only users
+// @tested 	Yes
+// TODO: add conditions to check userRole and limit 'createWishCard' access to 'partners' only
+router.get('/profile/:id', async (req, res) => {
+    try {
+		var userId = req.params.id;
+		const userData = await User.findOne({_id: userId});
+		res.render((path.join( __dirname, '../public', 'profile.html' )), {user: userData});
+    } catch (error) {
+        res.status(400).send(JSON.stringify({
+			success: false,
+			error: err
+		}));
+	}
+});
 
-router
-    .route('/register')
-    .get(redirectProfile, getUsersRegister);
 
-router
-    .route('/login')
-    .post(redirectProfile, postUsersLogin);
+// @desc    Create a newUser, hash password, issue session
+// @route   POST '/users/signup'
+// @access  Public
+// @tested 	Yes
+// TODO: display this message in signup.html client side as a notification alert
+router.post('/signup', async (req, res) => {
+	const { fName, lName, email, password, userRole} = req.body;
+	const candidate = await User.findOne({email: email});
+	if (candidate) {
+		return res.status(409).send('This email is already taken. Try another');
+	} else {
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+		const newUser = new User({
+			fName,
+			lName,
+			email,
+			password: hashedPassword,
+			userRole
+		});
+		var userId = mongoose.Types.ObjectId(newUser._id);
+		req.session.userId = userId;
+		try {
+			console.log(newUser); 
+			await newUser.save();
+			return res.redirect(`/users/profile/${userId}`);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+});
 
-router
-    .route('/register')
-    .post(redirectProfile, postUsersRegister);
+// @desc    Render login.html
+// @route   POST '/users/login'
+// @access  Public
+// @tested 	Not yet
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+	const user = await User.findOne({ email: email });
+	if (user) {
+		if (await bcrypt.compare(password, user.password)) {
+			req.session.userId = user.id;
+			return res.redirect(`/users/profile/${user._id}`);
+		}
+	}
+	res.redirect('/users/login');
+});
 
-router
-    .route('/logout')
-    .post(redirectLogin, postUsersLogout);    
+// @desc    Render login.html
+// @route   POST '/users/logout'
+// @access  Public
+// @tested 	Not yet
+// TODO: connect this route with the 'Log Out' button
+router.post('/logout', (req, res) => {
+    req.session.destroy(err => {
+		if (err) {
+			return res.redirect('/users/profile');
+		}
+		res.clearCookie(process.env.SESS_NAME);
+		res.redirect('/users/login');
+	});
+});
 
 module.exports = router;
+
+
+// const redirectLogin = (req, res, next) => {
+//     if (!req.session.userId) {
+//         res.redirect('/login');
+//     } else {
+//         next();
+//     }
+// }
+
+// const redirectProfile = (req, res, next) => {
+//     if (req.session.userId) {
+//         res.redirect('/profile');
+//     } else {
+//         next();
+//     }
+// }
+
+// const {
+//     getUsersRoot,
+//     getUsersProfile,
+//     getUsersLogin,
+//     getUsersRegister,
+//     postUsersLogin,
+//     postUsersRegister,
+//     postUsersLogout
+// } = require('../controllers/users');
+
+// const User = require('../models/User');
+// const { Router } = require('express');
+
+// const router = express.Router({
+//     mergeParams: true
+// });
+
+
+// router
+//     .route('/')
+//     .get(getUsersRoot);
+
+// router
+//     .route('/profile')
+//     .get(redirectLogin, getUsersProfile);
+
+// router
+//     .route('/login')
+//     .get(redirectProfile, getUsersLogin);
+
+// router
+//     .route('/register')
+//     .get(redirectProfile, getUsersRegister);
+
+// router
+//     .route('/login')
+//     .post(redirectProfile, postUsersLogin);
+
+// router
+//     .route('/register')
+//     .post(redirectProfile, postUsersRegister);
+
+// router
+//     .route('/logout')
+//     .post(redirectLogin, postUsersLogout);
