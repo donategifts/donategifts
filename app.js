6c52f15806026118c61ee9e3cd7ef020620e8bbe
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-29 09:49:36 -0500

    Formatting/Error Fixes
    - Formatting fixes
    
    - Profile
    	- Added logic to display a message for unverified agencies
    	- Added logic to hide the create wishcard button for unverified agencies (form is already hidden, button wasn't)
    - Users route
    	- Changed the logic in get Profile
    		- Earlier version didn't handle all cases (ex: it only handled agency info for newly registered users)
    		- Current version takes care of both new and old users
    - App.js
    	- Middleware for agency info extraction is strictly not necessary (as of now)
    		- Since agency information is only displayed in Profile section

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 /*
  * Author: Stacy Sealky Lee
  * FileName: app.js
- * FileDescription: 
- * The set up codes are in an order, 
+ * FileDescription:
+ * The set up codes are in an order,
  * so some functions won't work if you switch the order of what gets loaded in app.js first.
- * 
+ *
  */
 
-//EXPRESS SET UP 
-const express = require('express');
+//EXPRESS SET UP
+const express = require("express");
 const app = express();
 
 //NPM DEPENDENCIES
-const bodyParser = require('body-parser');
-const path = require('path');
-const mongoose = require('mongoose');
-const MongoClient = require('mongodb').MongoClient;
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const dotenv = require('dotenv');
-const ejs = require('ejs');
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const dotenv = require("dotenv");
+const ejs = require("ejs");
 
 //SET VIEW ENGINE AND RENDER HTML WITH EJS
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.engine('html', ejs.renderFile);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.engine("html", ejs.renderFile);
 
 //STATIC SET UP
-app.use(express.static('./public'));
-app.use('/wishcards/uploads', express.static('./uploads'));
-app.use('/uploads', express.static('./uploads'));
+app.use(express.static("./public"));
+app.use("/wishcards/uploads", express.static("./uploads"));
+app.use("/uploads", express.static("./uploads"));
 
 //DEV ENV
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 8081;
 // LIVE ENV
 // const hostname = '157.245.243.18';
@@ -41,83 +41,85 @@ const port = 8081;
 
 //LOAD CONFIG.ENV VARS
 dotenv.config({
-	path: './config/config.env'
+  path: "./config/example-config.env",
 });
 
 //DB SET UP & APP LISTEN (server starts after db connection)
 var options = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 mongoose.Promise = global.Promise;
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 mongoose.connect(process.env.MONGO_URI, options, (err, database) => {
-	if (err) {
-		console.log('Unable to connect to DB. Error:', err);
-	} else {
-		console.log('Connected to Mongodb');
-		app.listen(port, hostname, () => {
-			console.log(`Server running at http://${hostname}:${port}/`);
-		});
-	}
+  if (err) {
+    console.log("Unable to connect to DB. Error:", err);
+  } else {
+    console.log("Connected to Mongodb");
+    app.listen(port, hostname, () => {
+      console.log(`Server running at http://${hostname}:${port}/`);
+    });
+  }
 });
 
 //IMPORT MODELS
-const User = require('./models/User');
+const User = require("./models/User");
 const Agency = require("./models/Agency");
 //SESSION SET UP
-app.use(session({
-	store: new MongoStore({
-		url: process.env.MONGO_URI,
-		clear_interval: 3600000
-	}),
-	name: process.env.SESS_NAME,
-	resave: false,
-	saveUninitialized: false,
-	secret: process.env.SESS_SECRET,
-	cookie: {
-		maxAge: Number(process.env.SESS_LIFE), // cookie set to expire in 1 hour 
-		sameSite: true,
-		secure: process.env.NODE_ENV === 'production'
-	}
-}));
+app.use(
+  session({
+    store: new MongoStore({
+      url: process.env.MONGO_URI,
+      clear_interval: 3600000,
+    }),
+    name: process.env.SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESS_SECRET,
+    cookie: {
+      maxAge: Number(process.env.SESS_LIFE), // cookie set to expire in 1 hour
+      sameSite: true,
+      secure: process.env.NODE_ENV === "production",
+    },
+  })
+);
 
 // MIDDLEWARE for extracting userId from a session
 app.use(async (req, res, next) => {
-	const {
-		userId, agencyId
-	} = req.session;
-	if (userId) {
-		const result = await User.findById(userId);
-		res.locals.user = result;
-	}
-	if (agencyId) {
-		const result = await Agency.findById(agencyId);
-		res.locals.agency = result;
-	}
-	next();
+  const { userId, agencyId } = req.session;
+  if (userId) {
+    const result = await User.findById(userId);
+    res.locals.user = result;
+  }
+  if (agencyId) {
+    const result = await Agency.findById(agencyId);
+    res.locals.agency = result;
+  }
+  next();
 });
 
 //PARSERS SET UP
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true,
-	useUnifiedTopology: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    useUnifiedTopology: true,
+  })
+);
 app.use(cookieParser());
 
 //IMPORT ROUTE FILES
-const usersRoute = require('./routes/users');
-const wishCardsRoute = require('./routes/wishCards');
-const aboutRoute = require('./routes/about');
+const usersRoute = require("./routes/users");
+const wishCardsRoute = require("./routes/wishCards");
+const aboutRoute = require("./routes/about");
 
 //MOUNT ROUTERS
-app.use('/users', usersRoute);
-app.use('/wishcards', wishCardsRoute);
-app.use('/about', aboutRoute);
+app.use("/users", usersRoute);
+app.use("/wishcards", wishCardsRoute);
+app.use("/about", aboutRoute);
 
-app.get('/', (req, res) => {
-	res.render('home', {
-		user: res.locals.user
-	});
+app.get("/", (req, res) => {
+  res.render("home", {
+    user: res.locals.user,
+  });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,80 +1,86 @@
 //NPM DEPENDENCIES
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 
 //IMPORT USER MODEL
-const User = require('../models/User');
+const User = require("../models/User");
 
 //IMPORT AGENCY MODEL
-const Agency = require('../models/Agency');
+const Agency = require("../models/Agency");
 
-// Middleware for users 
+// Middleware for users
 const redirectLogin = (req, res, next) => {
-	if (!req.session.userId) {
-		res.redirect('/users/login');
-	} else {
-		next();
-	}
+  if (!req.session.userId) {
+    res.redirect("/users/login");
+  } else {
+    next();
+  }
 };
 const redirectProfile = (req, res, next) => {
-	if (req.session.userId) {
-		res.redirect(`/users/profile`);
-	} else {
-		next();
-	}
+  if (req.session.userId) {
+    res.redirect(`/users/profile`);
+  } else {
+    next();
+  }
 };
 
 // @desc    Render (home)
 // @route   GET '/users'
 // @access  Public
 // @tested 	Yes
-router.get('/', (req, res) => {
-	try {
-		res.status(200).render('home', {
-			user: res.locals.user
-		});
-	} catch (error) {
-		res.status(400).send(JSON.stringify({
-			success: false,
-			error: err
-		}));
-	}
+router.get("/", (req, res) => {
+  try {
+    res.status(200).render("home", {
+      user: res.locals.user,
+    });
+  } catch (error) {
+    res.status(400).send(
+      JSON.stringify({
+        success: false,
+        error: err,
+      })
+    );
+  }
 });
 
 // @desc    Render signup.html
 // @route   GET '/users/signup'
 // @access  Public
 // @tested 	Yes
-router.get('/signup', redirectProfile, (req, res) => {
-	try {
-		res.status(200).render('signup', {
-			user: res.locals.user
-		});
-	} catch (error) {
-		res.status(400).send(JSON.stringify({
-			success: false,
-			error: err
-		}));
-	}
+router.get("/signup", redirectProfile, (req, res) => {
+  try {
+    res.status(200).render("signup", {
+      user: res.locals.user,
+    });
+  } catch (error) {
+    res.status(400).send(
+      JSON.stringify({
+        success: false,
+        error: err,
+      })
+    );
+  }
 });
 
-// @desc    
+// @desc
 // @route   GET '/users/login'
 // @access  Private
 // @tested 	yes
-router.get('/login', redirectProfile, (req, res) => {
-	try {
-		res.status(200).render('login', {
-			user: res.locals.user
-		});
-	} catch (error) {
-		res.status(400).send(JSON.stringify({
-			success: false,
-			error: err
-		}));
-	}
+router.get("/login", redirectProfile, (req, res) => {
+  try {
+    res.status(200).render("login", {
+      user: res.locals.user,
+    });
+  } catch (error) {
+    res.status(400).send(
+      JSON.stringify({
+        success: false,
+        error: err,
+      })
+    );
+  }
 });
 
 // @desc    Render profile.html, grabs userId and render ejs data in static template
@@ -82,195 +88,190 @@ router.get('/login', redirectProfile, (req, res) => {
 // @access  Private, only users
 // @tested 	Yes
 // TODO: add conditions to check userRole and limit 'createWishCard' access to 'partners' only
-router.get('/profile', redirectLogin, async (req, res) => {
-	try {
-		let params = { user: res.locals.user };
-		if (res.locals.agency)
-			params = {...params, agency: res.locals.agency}
-		res.render('profile', params);
-	} catch (err) {
-		res.status(400).send(JSON.stringify({
-			success: false,
-			error: err
-		}));
-	}
+router.get("/profile", redirectLogin, async (req, res) => {
+  try {
+    let user = res.locals.user;
+    let params = { user };
+    if (user.userRole == "partner") {
+      let agency = await Agency.findOne({ accountManager: user._id });
+      params = { ...params, agency };
+    }
+    res.render("profile", params);
+  } catch (err) {
+    res.status(400).send(
+      JSON.stringify({
+        success: false,
+        error: err,
+      })
+    );
+  }
 });
 
 // @desc    Update user about me info
 // @route   PUT '/users/profile'
 // @access  Private, only users
 // @tested 	No?
-router.put('/profile', redirectLogin, async (req, res) => {
-	try {
-        const {aboutMe} = req.body;
-        
-        // if no user id is present return forbidden status 403
-        if (!req.session.userId) {
-            res.status(403).send(JSON.stringify({
-                success: false,
-                error: "No user id in request"
-            }));
-        }
+router.put("/profile", redirectLogin, async (req, res) => {
+  try {
+    const { aboutMe } = req.body;
 
-        const candidate = await User.findOne({_id: req.session.userId});
+    // if no user id is present return forbidden status 403
+    if (!req.session.userId) {
+      res.status(403).send(
+        JSON.stringify({
+          success: false,
+          error: "No user id in request",
+        })
+      );
+    }
 
-        // candidate with id not found in database, return not found status 404
-        if (!candidate) {
-            res.status(404).send(JSON.stringify({
-                success: false,
-                error: "User could not be found"
-            }));
-        }
-        
-        // update user and add aboutMe
-        User.updateOne(
-            {_id: candidate._id}, 
-            {aboutMe : aboutMe },
-            {multi:true}, 
-              function(err, numberAffected){  
-              });
+    const candidate = await User.findOne({ _id: req.session.userId });
 
-        res.status(200).send(JSON.stringify({
-            success: true,
-            error: null,
-            data: aboutMe,
-        }));
+    // candidate with id not found in database, return not found status 404
+    if (!candidate) {
+      res.status(404).send(
+        JSON.stringify({
+          success: false,
+          error: "User could not be found",
+        })
+      );
+    }
 
-	} catch (err) {
-		res.status(400).send(JSON.stringify({
-			success: false,
-			error: err
-		}));
-	}
+    // update user and add aboutMe
+    User.updateOne(
+      { _id: candidate._id },
+      { aboutMe: aboutMe },
+      { multi: true },
+      function (err, numberAffected) {}
+    );
+
+    res.status(200).send(
+      JSON.stringify({
+        success: true,
+        error: null,
+        data: aboutMe,
+      })
+    );
+  } catch (err) {
+    res.status(400).send(
+      JSON.stringify({
+        success: false,
+        error: err,
+      })
+    );
+  }
 });
-
 
 // @desc    Render agency.ejs
 // @route   GET '/users/agency'
 // @access  Private, only userRole == partners
 // @tested 	No
-router.get('/agency', redirectLogin, async (req, res) => {
-	try {
-		res.render('agency', {
-			user: res.locals.user
-		});
-	} catch (err) {
-		res.status(400).send(JSON.stringify({
-			success: false,
-			error: err
-		}));
-	}
+router.get("/agency", redirectLogin, async (req, res) => {
+  try {
+    res.render("agency", {
+      user: res.locals.user,
+    });
+  } catch (err) {
+    res.status(400).send(
+      JSON.stringify({
+        success: false,
+        error: err,
+      })
+    );
+  }
 });
-
 
 // @desc    agency info is sent to db
 // @route   POST '/users/agency'
 // @access  private, partners only
 // @tested 	No
-router.post('/agency', async (req, res) => {
-	const {
-		agencyName,
-		agencyWebsite,
-		agencyPhone,
-		agencyBio,
-	} = req.body;
+router.post("/agency", async (req, res) => {
+  const { agencyName, agencyWebsite, agencyPhone, agencyBio } = req.body;
 
-	const newAgency = new Agency({
-		agencyName,
-		agencyWebsite,
-		agencyPhone,
-		agencyBio,
-		accountManager: req.session.userId
-	});
-	try {
-		await newAgency.save();
-		var agencyId = mongoose.Types.ObjectId(newAgency._id);
-    	req.session.agencyId = agencyId;
-		console.log("agency data saved");
-		return res.send('/users/profile');
-	} catch (err) {
-		console.log(err);
-	}
+  const newAgency = new Agency({
+    agencyName,
+    agencyWebsite,
+    agencyPhone,
+    agencyBio,
+    accountManager: req.session.userId,
+  });
+  try {
+    await newAgency.save();
+    var agencyId = mongoose.Types.ObjectId(newAgency._id);
+    req.session.agencyId = agencyId;
+    console.log("agency data saved");
+    return res.send("/users/profile");
+  } catch (err) {
+    console.log(err);
+  }
 });
-
 
 // @desc    Create a newUser, hash password, issue session
 // @route   POST '/users/signup'
 // @access  Public
 // @tested 	Yes
 // TODO: display this message in signup.html client side as a notification alert
-router.post('/signup', redirectProfile, async (req, res) => {
-	const {
-		fName,
-		lName,
-		email,
-		password,
-		userRole
-	} = req.body;
-	const candidate = await User.findOne({
-		email: email
-	});
-	if (candidate) {
-		return res.status(409).send('This email is already taken. Try another');
-	} else {
-		const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(password, salt);
-		const newUser = new User({
-			fName,
-			lName,
-			email,
-			password: hashedPassword,
-			userRole
-		});
-		var userId = mongoose.Types.ObjectId(newUser._id);
-		req.session.userId = userId;
-		try {
-			await newUser.save();
-			//trying to add a second step here
-			//if the userRole is partner then redirect to agency.ejs then profile.ejs
-			if (newUser.userRole == 'partner') {
-				return res.send('/users/agency');
-			} else {
-				return res.send('/users/profile');
-			}
-		} catch (err) {
-			console.log(err);
-		}
-	}
+router.post("/signup", redirectProfile, async (req, res) => {
+  const { fName, lName, email, password, userRole } = req.body;
+  const candidate = await User.findOne({
+    email: email,
+  });
+  if (candidate) {
+    return res.status(409).send("This email is already taken. Try another");
+  } else {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      fName,
+      lName,
+      email,
+      password: hashedPassword,
+      userRole,
+    });
+    var userId = mongoose.Types.ObjectId(newUser._id);
+    req.session.userId = userId;
+    try {
+      await newUser.save();
+      //trying to add a second step here
+      //if the userRole is partner then redirect to agency.ejs then profile.ejs
+      if (newUser.userRole == "partner") {
+        return res.send("/users/agency");
+      } else {
+        return res.send("/users/profile");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 });
-
-
 
 // @desc    Render login.html
 // @route   POST '/users/login'
 // @access  Public
 // @tested 	Not yet
-router.post('/login', redirectProfile, async (req, res) => {
-	const {
-		email,
-		password
-	} = req.body;
-	const user = await User.findOne({
-		email: email
-	});
-	if (user) {
-		if (await bcrypt.compare(password, user.password)) {
-			req.session.userId = user.id;
-			return res.redirect('/users/profile');
-		}
-	}
-	res.redirect('/users/login');
+router.post("/login", redirectProfile, async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    email: email,
+  });
+  if (user) {
+    if (await bcrypt.compare(password, user.password)) {
+      req.session.userId = user.id;
+      return res.redirect("/users/profile");
+    }
+  }
+  res.redirect("/users/login");
 });
 
 // @desc    Render login.html
 // @route   GET '/users/logout'
 // @access  Public
 // @tested 	Not yet
-router.get('/logout', redirectLogin, (req, res) => {
-	req.session.destroy(err => {
-		res.clearCookie(process.env.SESS_NAME);
-		res.redirect('/users/login');
-	});
+router.get("/logout", redirectLogin, (req, res) => {
+  req.session.destroy((err) => {
+    res.clearCookie(process.env.SESS_NAME);
+    res.redirect("/users/login");
+  });
 });
 
 module.exports = router;
