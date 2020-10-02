4a6cf7792f34962bad33f2a6c84e6e6f26af0684
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-10-02 14:44:15 -0500

    Refactoring/Error fixes
    - Slight error in homeSampleCards.ejs
    	-  for childInterest template variable
    - Slight refactor of some of the routes (easier to read)
    	- Handled some error cases as well (ex: when wishcards/messages are empty)

--- routes/wishCards.js
+++ routes/wishCards.js
@@ -75,21 +75,15 @@ router.post('/', upload.single('wishCardImage'), (req, res) => {
     );
   } else {
     var newCard = new WishCard({
-      childFirstName: req.body.childFirstName,
-      childLastName: req.body.childLastName,
       childBirthday: new Date(req.body.childBirthday),
-      childInterest: req.body.childInterest,
-      wishItemName: req.body.wishItemName,
       wishItemPrice: Number(req.body.wishItemPrice),
-      wishItemURL: req.body.wishItemURL,
-      childStory: req.body.childStory,
       wishCardImage: req.file.location,
       createdBy: res.locals.user._id,
+      ...req.body,
     });
     newCard.save((err) => {
       if (err) console.log(err);
     });
-    console.log(newCard);
     res.redirect('/wishcards');
   }
 });
@@ -142,21 +136,41 @@ router.post('/search', async (req, res) => {
 });
 
 // This needs to be moved elsewhere during Cleanup
-let getDefaultMessages = (userFirstName, childFirstName) => {
-  return [
-    `${userFirstName} sends you love, ${childFirstName}`,
-    'Happy Birthday to the sweetest kid in the entire world.',
-    'Happy birthday to a future superstar!',
-    `Happy birthday, ${childFirstName}`,
-    `Merry Christmas, ${childFirstName}`,
-    `Happy holidays, ${childFirstName}`,
-    `${childFirstName}, you are awesome!`,
-    `Lots of love and best wishes, ${childFirstName}`,
-    `${childFirstName}, hope you enjoy my gift!`,
-    'Merry Christmas and a Happy New Year',
-    `${childFirstName}, have a happy holiday`,
-    `Congratulations, ${childFirstName}`,
-  ];
+let getMessageChoices = (userFirstName, childFirstName) => {
+  if (!userFirstName | !childFirstName) {
+    return [];
+  } else {
+    return [
+      `${userFirstName} sends you love, ${childFirstName}`,
+      'Happy Birthday to the sweetest kid in the entire world.',
+      'Happy birthday to a future superstar!',
+      `Happy birthday, ${childFirstName}`,
+      `Merry Christmas, ${childFirstName}`,
+      `Happy holidays, ${childFirstName}`,
+      `${childFirstName}, you are awesome!`,
+      `Lots of love and best wishes, ${childFirstName}`,
+      `${childFirstName}, hope you enjoy my gift!`,
+      'Merry Christmas and a Happy New Year',
+      `${childFirstName}, have a happy holiday`,
+      `Congratulations, ${childFirstName}`,
+    ];
+  }
+};
+
+let getPreviousMessages = async (wishcard) => {
+  if (wishcard.messages.length > 0) {
+    messages = await Promise.all(
+      wishcard.messages.map(async (messageID) => {
+        let foundMessage = await Message.findById(messageID);
+        let foundUser = await User.findById(foundMessage.messageFrom);
+        return {
+          message: foundMessage.message,
+          fromUser: foundUser.fName,
+        };
+      })
+    );
+  }
+  return wishcard;
 };
 
 // @desc    Retrieve one wishcard by its _id
@@ -170,25 +184,16 @@ router.get('/:id', async (req, res) => {
   } else {
     try {
       let wishcard = await WishCard.findById(req.params.id);
-      let messages = [];
-      if (wishcard.messages.length > 0) {
-        messages = await Promise.all(
-          wishcard.messages.map(async (messageID) => {
-            let foundMessage = await Message.findById(messageID);
-            let foundUser = await User.findById(foundMessage.messageFrom);
-            return { message: foundMessage.message, fromUser: foundUser.fName };
-          })
-        );
-      }
-      let defaultMessages = getDefaultMessages(
+      wishcard = await getPreviousMessages(wishcard);
+      let defaultMessages = getMessageChoices(
         res.locals.user.fName,
         wishcard.childFirstName
       );
       // create a page and have a dynamic link for see more
       res.status(200).render('wishCardFullPage', {
         user: res.locals.user,
-        wishcard,
-        messages,
+        wishcard: wishcard ? wishcard : [],
+        messages: messages ? messages : [],
         defaultMessages,
       });
     } catch (error) {
