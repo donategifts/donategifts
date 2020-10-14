a2fb28d66920cfca602f0f4b662f0081c2a8b13c
Tokikko <tokikko.zero@gmail.com>
2020-10-14 22:21:14 +0200

    Test enviroment
    If test enviroment always return true for captcha

--- routes/validations/googleReCaptcha.js
+++ routes/validations/googleReCaptcha.js
@@ -3,6 +3,9 @@ const superagent = require('superagent');
 // check if captcha token is valid
 // returns boolean 
 async function validateReCaptchaToken(token) {
+    if (process.env.NODE_ENV === 'test') {
+        return true;
+    }
     let googleUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_CAPTCHA_KEY}&response=${token}`;
     return (async () => {
         try {
