e501f007047dfb6b08ca72fb5c0e9ac98f5c9ab0
gnorbsl <6427935+gnorbsl@users.noreply.github.com>
2020-10-18 22:07:10 +0200

    Update socket.js

--- server/helper/socket.js
+++ server/helper/socket.js
@@ -1,3 +1,13 @@
-const io = require('socket.io')(3000);
+const io = require('socket.io')(3000, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 
 module.exports = io
