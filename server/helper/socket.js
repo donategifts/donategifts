555bf970bbf43b8f789c738519dd4dacf83a4dce
gnorbsl <6427935+gnorbsl@users.noreply.github.com>
2020-10-19 11:42:18 +0200

    Update socket.js

--- server/helper/socket.js
+++ server/helper/socket.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const https = require('https');
 const options = {
-  key: fs.readFileSync('etc/letsencrypt/live/wsdev.donate-gifts.com/privkey.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/wsdev.donate-gifts.com/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/wsdev.donate-gifts.com/cert.pem')
 };
 var server = https.createServer(options);
