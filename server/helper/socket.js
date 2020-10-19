207297809813f3df9380d17714affd8ce0d52a4d
gnorbsl <6427935+gnorbsl@users.noreply.github.com>
2020-10-19 11:50:17 +0200

    Update socket.js

--- server/helper/socket.js
+++ server/helper/socket.js
@@ -6,7 +6,17 @@ const options = {
   cert: fs.readFileSync('/etc/letsencrypt/live/dev.donate-gifts.com/cert.pem')
 };
 var server = https.createServer(options);
-var io  = require('socket.io').listen(server);
+var io  = require('socket.io')(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 var port = 3000; // Enter any of the cloudflare ports.
 
 
