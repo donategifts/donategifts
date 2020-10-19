ef959d3307557b962c3974b86da5ef991144b923
gnorbsl <6427935+gnorbsl@users.noreply.github.com>
2020-10-19 11:14:49 +0200

    Update wishCard.js

--- client/public/clientJS/wishCard.js
+++ client/public/clientJS/wishCard.js
@@ -15,7 +15,7 @@ function preventChildImageContextMenu(e) {
     }
 }
 
-const socket = io('wsdev.donate-gifts.com');
+const socket = io('ws://wsdev.donate-gifts.com');
 let x = [];
 socket.on('block', event => {
 
