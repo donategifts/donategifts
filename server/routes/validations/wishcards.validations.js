2f4971e3d0fb37f84ea667ffb07188f1ec7f7b4f
Enubia <marcoschuster@mailbox.org>
2020-10-21 22:03:49 +0200

    fix: fixed toast messages on error

--- server/routes/validations/wishcards.validations.js
+++ server/routes/validations/wishcards.validations.js
@@ -1,8 +1,7 @@
 const { body, validationResult } = require('express-validator');
 const { handleError } = require('../../helper/error');
-const { log } = require('../../helper/logger');
 
-const createWishcardValidationRules = () => {
+const createWishCardValidationRules = () => {
   return [
     body('childBirthday').notEmpty().isString(),
     body('wishItemPrice').notEmpty().isString(),
@@ -19,13 +18,12 @@ const createWishcardValidationRules = () => {
 const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    log(res, errors);
     return handleError(res, 400, errors.array({ onlyFirstError: true })[0]);
   }
   next();
 };
 
 module.exports = {
-  createWishcardValidationRules,
+  createWishCardValidationRules,
   validate,
 };
