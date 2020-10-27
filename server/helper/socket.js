83e58346068846c0c8d83bdf7688bc5eca1ad2c5
gnorbsl <supersecret@email.de>
2020-10-27 21:52:42 +0100

    Update socket.js

--- server/helper/socket.js
+++ server/helper/socket.js
@@ -19,7 +19,7 @@ if (process.env.LOCAL_DEVELOPMENT) {
     handlePreflightRequest: (req, res) => {
       const headers = {
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        'Access-Control-Allow-Origin': req.headers.origin, // or the specific origin you want to give access to,
+        'Access-Control-Allow-Origin': 'localhost:3000 127.0.0.1:3000 https://dev.donate-gifts.com:3000 https://donate-gifts.com:3000', // or the specific origin you want to give access to,
         'Access-Control-Allow-Credentials': true,
       };
       res.writeHead(200, headers);
