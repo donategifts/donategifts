e4a662fa2b5619823b3bc6d7b0b38eb5f13f84d6
Tokikko <tokikko.zero@gmail.com>
2020-10-30 17:45:14 +0100

    Checkbox for agency address
    Added checkbox on wishcard creation. If users select it it will prefill wishcard address form with agency address

--- client/public/clientJS/profile.js
+++ client/public/clientJS/profile.js
@@ -58,6 +58,65 @@ $(document).ready(() => {
 // About me edit. Click handler
 window.onload = function () {
   let buttonSaveProfile = document.getElementById('save-about-me-button');
+  let agencyAddressCheckBox = document.getElementById('agencyAddressCheckBox');
+
+  buttonSaveProfile.addEventListener('click', handleSaveAboutMeButtonClick);
+  agencyAddressCheckBox.addEventListener('click', handleCheckBoxClick);
+
+  function handleCheckBoxClick() {
+    if (agencyAddressCheckBox.checked) {
+      // if the address is already filled with data ask user to clear it first
+      // so that we dont accidentally overwrite it
+      if (checkIfAgencyAddressFormIsEmpty()) {
+        fetch('/users/agency/address')
+        .then((response) => response.json())
+        .then((data) => {
+          const agency = data.data;
+          if (agency.agencyAddress) {
+            updateAgencyAddressForm(agency);
+          }
+          else {
+            showToast("Agency address not present. Most likely that information is missing, contact administrator");
+          }
+        })
+        .catch((error) => {
+          showToast("Could not get agency address");
+        })
+      }
+      else {
+        showToast("Address form must be empty if we want to use agency address so that there is no accidental overwrite");
+      }
+    }
+  }
+
+  // get html nodes and input with agency details from backend
+  function updateAgencyAddressForm(agencyDetails) {
+    // get all address input elements
+    let address1 = document.getElementById('address1');
+    let address2 = document.getElementById('address2');
+    let zipCode = document.getElementById('address_zip');
+    let city = document.getElementById('address_city');
+    let country = document.getElementById('address_country');
+    let state = document.getElementById('address_state');
+    // set new values
+    address1.value = agencyDetails.agencyAddress.address1;
+    address2.value = agencyDetails.agencyAddress.address2;
+    zipCode.value = agencyDetails.agencyAddress.zipcode;
+    city.value = agencyDetails.agencyAddress.city;
+    country.value = agencyDetails.agencyAddress.country;
+    state.value = agencyDetails.agencyAddress.state;
+  }
+
+  function checkIfAgencyAddressFormIsEmpty() {
+    let address1 = document.getElementById('address1');
+    let address2 = document.getElementById('address2');
+    let zipCode = document.getElementById('address_zip');
+    let city = document.getElementById('address_city');
+    let country = document.getElementById('address_country');
+    let state = document.getElementById('address_state');
+    // if length is zero, input fields are empty
+    return (address1.value.length || address2.value.length || zipCode.value.length || city.value.length || country.value.length || state.value.length) === 0;
+  }
 
   // button listener
   function handleSaveAboutMeButtonClick() {
@@ -94,5 +153,5 @@ window.onload = function () {
       });
   }
 
-  buttonSaveProfile.addEventListener('click', handleSaveAboutMeButtonClick);
+  
 };
