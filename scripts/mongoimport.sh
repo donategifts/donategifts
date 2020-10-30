fe53eb8d80ae7e5adf36608fdda72b73072c4be4
Enubia <marcoschuster@mailbox.org>
2020-10-31 00:38:02 +0100

    chore: update mongo script

--- scripts/mongoimport.sh
+++ scripts/mongoimport.sh
@@ -1,11 +1,12 @@
 # !/usr/bin/bash
 
 # run chmod +x mongoimport.sh to make it executable
+# run ./mongoimport.sh <test db url> as argument
 
-mongoexport --uri="<test db url>"  --collection=users  --out=users.json
-mongoexport --uri="<test db url>"  --collection=agencies  --out=agencies.json
-mongoexport --uri="<test db url>"  --collection=messages  --out=messages.json
-mongoexport --uri="<test db url>"  --collection=wishcards  --out=wishcards.json
+mongoexport --uri="$1"  --collection=users  --out=users.json
+mongoexport --uri="$1"  --collection=agencies  --out=agencies.json
+mongoexport --uri="$1"  --collection=messages  --out=messages.json
+mongoexport --uri="$1"  --collection=wishcards  --out=wishcards.json
 
 mongoimport --uri="mongodb://localhost/donategifts" --collection=users --file=users.json
 mongoimport --uri="mongodb://localhost/donategifts" --collection=agencies --file=agencies.json
