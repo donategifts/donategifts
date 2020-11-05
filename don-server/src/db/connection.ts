4aa7222c4dee9e0e6ca133a4e937648eae4748cb
Enubia <marcoschuster@mailbox.org>
2020-11-05 10:30:11 +0100

    refactor: try catch for mongo connection

--- don-server/src/db/connection.ts
+++ don-server/src/db/connection.ts
@@ -14,50 +14,54 @@ export default class MongooseConnection {
       );
     }
 
-    mongoose.set('useCreateIndex', true);
+    try {
+      mongoose.set('useCreateIndex', true);
 
-    mongoose.Model.on('index', (err) => {
-      if (err) {
-        logger.error('Mongoose create index error', err);
-      }
-    });
+      mongoose.Model.on('index', (err) => {
+        if (err) {
+          logger.error('Mongoose create index error', err);
+        }
+      });
 
-    // When successfully connected
-    mongoose.connection.on('connected', () => {
-      logger.info('Mongoose default connection open');
-    });
+      // When successfully connected
+      mongoose.connection.on('connected', () => {
+        logger.info('Mongoose default connection open');
+      });
 
-    // When destroyed
-    mongoose.connection.on('destroy', () => {
-      logger.info('Mongoose default connection destroyed');
-    });
+      // When destroyed
+      mongoose.connection.on('destroy', () => {
+        logger.info('Mongoose default connection destroyed');
+      });
 
-    // When successfully connected
-    mongoose.connection.on('reconnected', () => {
-      logger.info('Mongoose default connection reconnected');
-    });
+      // When successfully connected
+      mongoose.connection.on('reconnected', () => {
+        logger.info('Mongoose default connection reconnected');
+      });
 
-    // If the connection throws an error
-    mongoose.connection.on('error', (err) => {
-      logger.info(`Mongoose default connection error: ${err}`);
-    });
+      // If the connection throws an error
+      mongoose.connection.on('error', (err) => {
+        logger.info(`Mongoose default connection error: ${err}`);
+      });
 
-    // If the connection throws an error
-    mongoose.connection.on('index', (err) => {
-      if (err) {
-        logger.error(`Mongoose indexing error: ${err}`);
-      }
-    });
+      // If the connection throws an error
+      mongoose.connection.on('index', (err) => {
+        if (err) {
+          logger.error(`Mongoose indexing error: ${err}`);
+        }
+      });
 
-    // When the connection is disconnected
-    mongoose.connection.on('disconnected', () => {
-      logger.info('Mongoose default connection disconnected');
-    });
+      // When the connection is disconnected
+      mongoose.connection.on('disconnected', () => {
+        logger.info('Mongoose default connection disconnected');
+      });
 
-    await mongoose.connect(String(process.env.MONGO_URI), {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
+      await mongoose.connect(String(process.env.MONGO_URI), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      });
+    } catch (error) {
+      throw new Error(`Something went horrible wrong with Mongoose: ${error}`);
+    }
   }
 }
