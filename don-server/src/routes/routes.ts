9325abd7f65e40dc7520489cdf960056b151d800
Enubia <marcoschuster@mailbox.org>
2020-11-07 18:29:24 +0100

    new routes

--- don-server/src/routes/routes.ts
+++ don-server/src/routes/routes.ts
@@ -113,7 +113,7 @@ export function RegisterRoutes(app: express.Router) {
             promiseHandler(controller, promise, response, next);
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.post('/api/users/sig-nup',
+        app.post('/api/users/sign-up',
             function (request: any, response: any, next: any) {
             const args = {
                     _body: {"in":"body","name":"_body","required":true,"dataType":"any"},
