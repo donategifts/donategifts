5fb96517713cffab45288db0fda1d54fef8096a0
gnorbsl <supersecret@email.de>
2020-11-17 15:34:18 +0100

    fix clickstacking and logger

--- server/helper/logger.js
+++ server/helper/logger.js
@@ -1,18 +1,22 @@
 const { configure, getLogger, levels } = require('log4js');
 
+let appenderArray = ['console'];
+if (!!process.env.LOCAL_DEVELOPMENT) appenderArray.push('logstash');
+
+console.log(appenderArray)
 configure({
   appenders: {
     console: { type: 'console', layout: { type: 'colored' } },
     logstash: {
-      type: '@log4js-node/logstashudp',
-      host: 'localhost',
+      type: "log4js-logstash-tcp",
+      host: "localhost",
       port: 5000,
     },
   },
   categories: {
     default: { appenders: ['console'], level: levels.ALL },
-    development: { appenders: ['console', 'logstash'], level: levels.ALL },
-    production: { appenders: ['console', 'logstash'], level: levels.ALL },
+    development: { appenders: appenderArray, level: levels.ALL },
+    production: { appenders: appenderArray, level: levels.ALL },
 
   },
 
