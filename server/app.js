590a1239a47fe50fac3a8da4a46757e03a8b4c5b
Marco Schuster <lettucekiing@gmail.com>
2020-11-18 13:01:22 +0100

    feat: added load more, refined wishcard search (#205)
    * feat: added load more, refined wishcard search
    
    * fix: tests

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ dotenv.config({
 // EXPRESS SET UP
 const express = require('express');
 const bodyParser = require('body-parser');
-const responseTime = require('response-time')
+const responseTime = require('response-time');
 const requestIp = require('request-ip');
 
 const path = require('path');
@@ -59,32 +59,33 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(
+  responseTime((req, res, time) => {
+    if (
+      (!req.originalUrl.includes('.png') &&
+        !req.originalUrl.includes('.jpg') &&
+        !req.originalUrl.includes('.js') &&
+        !req.originalUrl.includes('.svg') &&
+        !req.originalUrl.includes('.jpeg') &&
+        !req.originalUrl.includes('.woff') &&
+        !req.originalUrl.includes('.css') &&
+        !req.originalUrl.includes('.ico')) ||
+      res.statusCode > 304
+    ) {
+      const clientIp = requestIp.getClientIp(req);
 
-app.use(responseTime(function (req, res, time) {
-
-  if (!req.originalUrl.includes('.png')
-    && !req.originalUrl.includes('.jpg')
-    && !req.originalUrl.includes('.js')
-    && !req.originalUrl.includes('.svg')
-    && !req.originalUrl.includes('.jpeg')
-    && !req.originalUrl.includes('.woff')
-    && !req.originalUrl.includes('.css')
-    && !req.originalUrl.includes('.ico')
-    || (res.statusCode > 304) ) {
-
-    const clientIp = requestIp.getClientIp(req);
-
-    log.info('New request',{
-      type: 'request',
-      user: res.locals.user?String(res.locals.user._id).substring(0,10):'guest',
-      method: req.method,
-      statusCode: res.statusCode,
-      route: req.originalUrl,
-      responseTime: Math.ceil(time),
-      ip: clientIp
-    })
-  }
-}))
+      log.info('New request', {
+        type: 'request',
+        user: res.locals.user ? String(res.locals.user._id).substring(0, 10) : 'guest',
+        method: req.method,
+        statusCode: res.statusCode,
+        route: req.originalUrl,
+        responseTime: Math.ceil(time),
+        ip: clientIp,
+      });
+    }
+  }),
+);
 // mongo connection needs to be established before admin-bro setup
 MongooseConnection.connect();
 
