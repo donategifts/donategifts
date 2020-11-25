664220598105faa725eeec6e5c040a786f260d70
Tokikko <tokikko.zero@gmail.com>
2020-11-25 23:11:29 +0100

    Disable stripe button immediatelly after click (#222)

--- client/public/clientJS/stripe.js
+++ client/public/clientJS/stripe.js
@@ -27,13 +27,16 @@ card.on('change', function (event) {
 let form = document.getElementById('payment-form');
 form.addEventListener('submit', function (event) {
   event.preventDefault();
+  loading(true);
   stripe.createToken(card).then(function (result) {
     if (result.error) {
       // Inform the user if there was an error.
       let errorElement = document.getElementById('card-errors');
       errorElement.textContent = result.error.message;
+      loading(false);
     } else {
       // Send the token to your server.
+      
       let wishCardId = document.getElementById('cardId');
       let agencyName = document.getElementById('agencyName');
       fetch('/stripe/createIntent', {
@@ -53,6 +56,7 @@ form.addEventListener('submit', function (event) {
         .catch((error) => {
           console.error('Error:', error);
           showToast('An error has occured while processing payment');
+          loading(false);
         });
     }
   });
@@ -78,11 +82,11 @@ let payWithCard = function (stripe, card, clientSecret) {
 let loading = function (isLoading) {
   if (isLoading) {
     // Disable the button and show a spinner
-    document.querySelector('button').disabled = true;
+    document.getElementById('submit').disabled = true;
     document.querySelector('#spinner').classList.remove('hidden');
     document.querySelector('#button-text').classList.add('hidden');
   } else {
-    document.querySelector('button').disabled = false;
+    document.getElementById('submit').disabled = false;
     document.querySelector('#spinner').classList.add('hidden');
     document.querySelector('#button-text').classList.remove('hidden');
   }
@@ -90,9 +94,6 @@ let loading = function (isLoading) {
 
 let orderComplete = function (paymentIntentId) {
   loading(false);
-  document
-    .querySelector('.result-message a')
-    .setAttribute('href', 'https://dashboard.stripe.com/test/payments/' + paymentIntentId);
   document.querySelector('.result-message').classList.remove('hidden');
   document.querySelector('#submit').setAttribute('disabled', 'true');
   showToast('Payment successfull', 'green');
@@ -100,7 +101,7 @@ let orderComplete = function (paymentIntentId) {
 
 let showError = function (errorMsgText) {
   loading(false);
-  let errorMsg = document.querySelector('#card-error');
+  let errorMsg = document.querySelector('#card-errors');
   errorMsg.textContent = errorMsgText;
   setTimeout(function () {
     errorMsg.textContent = '';
