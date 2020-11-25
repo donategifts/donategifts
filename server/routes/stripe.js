eb95531cf68d6cde9f6e84d12548ca8737a7bd84
Tokikko <tokikko.zero@gmail.com>
2020-11-25 12:31:11 +0100

    Stripe
    Add frontend stuff for stripe. Linked all wishcards to a new donate page. Missing design

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -6,6 +6,7 @@ const { redirectLogin } = require('./middleware/login.middleware');
 const WishCardRepository = require('../db/repository/WishCardRepository');
 const { sendDonationConfirmationMail } = require('../helper/messaging');
 const log = require('../helper/logger');
+const { calculateWishItemTotalPrice } = require('../helper/wishCard.helper');
 
 const router = express.Router();
 
@@ -15,14 +16,15 @@ router.post('/createIntent', redirectLogin, async (req, res) => {
   // Create a PaymentIntent with the order amount and currency
   const wishCard = await WishCardRepository.getWishCardByObjectId(mongoSanitize.sanitize(wishCardId));
 
+  // By default stripe accepts "pennies" and we are storing in a full "dollars". 1$ == 100
+  // so we need to multiple our price by 100. Genious explanation
+  const PENNY = 100;
+  const totalItemPrice = await calculateWishItemTotalPrice(wishCard.wishItemPrice);
+
   if (wishCard) {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: wishCard.wishItemPrice * 100,
+      amount: totalItemPrice * PENNY,
       currency: 'usd',
-      metadata: {
-        wishCardId: wishCard._id,
-        userId: res.locals.user._id,
-      },
     });
     res.send({
       clientSecret: paymentIntent.client_secret,
