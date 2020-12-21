d83cd16bbdd08883a90bca76e16f4789a6cdbb1b
enubia <marcoschuster@mailbox.org>
2020-12-21 18:00:14 +0100

    deps updgrade, gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -18,29 +18,6 @@ task('cleanDist', function () {
 	});
 });
 
-task('updatePackages', function (cb) {
-	const check = pkgJsonPath => {
-		try {
-			return childProcess
-				.execSync(`npx ncu --timeout 240000 -x ejs,webpack,socket.io,mongoose --packageFile ${pkgJsonPath} -u`)
-				.toString();
-		} catch (error) {
-			console.log(`exec error: ${error.message}`);
-			process.exit(error.status);
-		}
-	};
-	glob('./**/*/package.json', {}, (er, files) => {
-		files.forEach(file => {
-			if (file.includes('node_modules')) {
-				return;
-			}
-			// console.log(`command to update: ncu --packageFileDir --packageFile ${file} -u -i`);
-			console.log(check(file));
-		});
-		cb();
-	});
-});
-
 exports.superclean = parallel('cleanDist', 'cleanTmp', 'cleanModules');
 
 exports.clean = parallel('cleanDist', 'cleanTmp');
