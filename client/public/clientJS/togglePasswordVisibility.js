3ca32245c236090d232a3690377995fb5e5cd344
Tokikko <tokikko.zero@gmail.com>
2021-10-26 13:09:48 +0200

    changed icons to font awesome icons. Because they are awesome

--- client/public/clientJS/togglePasswordVisibility.js
+++ client/public/clientJS/togglePasswordVisibility.js
@@ -3,19 +3,19 @@ window.onload = function () {
   showPasswordImage.addEventListener('click', togglePasswordVisibility);
 
   function togglePasswordVisibility() {
-    const imageHidePath = '../public/img/pass-hide.png';
-    const imageVisiblePath = '../public/img/pass-unhide.png';
+    const imageHideClass = 'fas fa-eye';
+    const imageVisibleClass = 'fas fa-eye-slash';
     const passwordInput = document.getElementById('password');
     const passworInputType = passwordInput.type;
     switch (passworInputType) {
       case 'password':
         passwordInput.type = 'text'
-        showPasswordImage.src = imageHidePath
+        showPasswordImage.className = imageVisibleClass;
         break;
       
       case 'text':
         passwordInput.type = 'password';
-        showPasswordImage.src = imageVisiblePath;
+        showPasswordImage.className = imageHideClass;
         break;
       default:
         break;
