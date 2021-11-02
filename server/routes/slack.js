3e530cdff8ac8513ad9d6d3e4d36c192d1dc017c
Enubia <marcoschuster@mailbox.org>
2021-11-02 12:12:42 +0100

    added prettier plugin

--- server/routes/slack.js
+++ server/routes/slack.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const log = require('../helper/logger');
 const AgencyRepository = require('../db/repository/AgencyRepository');
-const { sendAgencyVerificationNotificationSuccess} = require('../helper/messaging');
+const { sendAgencyVerificationNotificationSuccess } = require('../helper/messaging');
 
 const router = express.Router();
 
@@ -10,15 +10,19 @@ router.post('/verify-agency', async (req, res) => {
 
   try {
     const agency = await AgencyRepository.verifyAgency(payload.actions.value);
-  
-    await sendAgencyVerificationNotificationSuccess({agency, user: payload.user.name, responseUrl: payload.response_url})
-  } catch(error) {
+
+    await sendAgencyVerificationNotificationSuccess({
+      agency,
+      user: payload.user.name,
+      responseUrl: payload.response_url,
+    });
+  } catch (error) {
     log.error(error);
     res.send({
-      "response_type": "ephemeral",
-      "replace_original": false,
-      "text": "Sorry, that didn't work. Please try again."
-    })
+      response_type: 'ephemeral',
+      replace_original: false,
+      text: "Sorry, that didn't work. Please try again.",
+    });
   }
 });
 
