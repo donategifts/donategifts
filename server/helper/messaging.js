32a463e17e5eeda4acc795f83485e8faa89e6907
patric <ph@mycraftnote.de>
2021-12-12 10:47:47 +0100

    fixed path and use gmail for dev

--- server/helper/messaging.js
+++ server/helper/messaging.js
@@ -98,7 +98,10 @@ const templateAttachments = [
 ];
 
 const getTransport = async () => {
-  if ((process.env.NODE_ENV === 'development' && process.env.LOCAL_DEVELOPMENT === 'TRUE') || process.env.NODE_ENV === 'test') {
+  if (
+    (process.env.NODE_ENV === 'development' && process.env.LOCAL_DEVELOPMENT === 'TRUE') ||
+    process.env.NODE_ENV === 'test'
+  ) {
     const account = await nodemailer.createTestAccount();
 
     if (account) {
