43b2f5398505eb94350d66f3bb6bbe6f7ae4efa0
patric <ph@mycraftnote.de>
2021-12-15 22:47:18 +0100

    feat/test_for_aws

--- server/helper/logger.js
+++ server/helper/logger.js
@@ -1,18 +1,11 @@
 const { configure, getLogger, levels } = require('log4js');
 
 const appenderArray = ['console'];
-if (process.env.NODE_ENV !== 'development') {
-  appenderArray.push('logstash');
-}
+
 
 const config = {
   appenders: {
     console: { type: 'console', layout: { type: 'colored' } },
-    logstash: {
-      type: 'log4js-logstash-tcp',
-      host: 'localhost',
-      port: 5044,
-    },
   },
   categories: {
     default: { appenders: ['console'], level: levels.ALL },
