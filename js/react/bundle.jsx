931d62d82deb64e0aa01536e500e6f11cc875d13
Nevena Milivojevic <NevMilivojevic@gmail.com>
2023-08-09 12:51:33 +0200

    Reusable empty modal (#606)
    Co-authored-by: homelyseven250 <georgee.keylock@outlook.com>

--- js/react/bundle.jsx
+++ js/react/bundle.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import Community from './components/community/index.jsx';
+// TBD import login when nav is converted to react component in order to control the open/close modal state
+// import Login from './components/login/index.jsx';
 import WishCards from './components/wishcards/index.jsx';
 
 window.ReactRoot = createRoot;
@@ -9,3 +11,4 @@ window.React = React;
 
 window.Community = Community;
 window.WishCards = WishCards;
+// window.Login = Login;
---
+++ js/react/components/login/index.jsx
@@ -0,0 +1,31 @@
+import Modal from '../shared/Modal.jsx';
+
+function Login() {
+	return (
+		<Modal
+			title={<h1 className="cool-font text-secondary">Welcome back</h1>}
+			body={
+				<div className="d-flex flex-column align-items-center justify-content-center gap-5 w-50">
+					<button>Google btn</button>
+					<button>Facebook btn</button>
+					<button className="btn-rounded-transparent w-100 d-flex justify-content-around align-items-center">
+						<span className="fa fa-envelope-o fs-3" />
+						<p className="m-0 fs-5 fw-bold">Log in with Email</p>
+					</button>
+				</div>
+			}
+			footer={<p>Agency partner users must sign in with work email</p>}
+			sideContent={
+				<div className="d-flex flex-column align-items-center justify-content-around text-center gap-4">
+					<div className="d-flex flex-column align-items-center justify-content-center text-white">
+						<h2 className="cool-font">No account?</h2>
+						<p className="fs-5 mt-1">Join our great movement</p>
+					</div>
+					<button className="fs-5 fw-bold">Sign up</button>
+				</div>
+			}
+		/>
+	);
+}
+
+export default Login;
