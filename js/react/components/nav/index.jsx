2bc0d29efe4d421eb0c08466cdcc7ba488c75179
NMilivojevic <nevmilivojevic@gmail.com>
2023-08-09 12:18:46 +0200

    convert nav into react component

---
+++ js/react/components/nav/index.jsx
@@ -0,0 +1,154 @@
+import PropTypes from 'prop-types';
+
+function Nav(props) {
+	const { user, agency } = props;
+
+	console.log('render nav', user, agency);
+
+	return (
+		<div className="w-100">
+			<div className="navbar navbar-expand-md mb-0 p-1 bg-white">
+				<div className="container my-0">
+					<a className="navbar-brand ms-3 m-lg-0 p-0" href="/" rel="home">
+						<img src="/img/new-donate-gifts-logo-2.png" alt="logo" />
+					</a>
+					<div className="collapse navbar-collapse justify-content-end">
+						<div className="navbar nav">
+							<div className="nav-item">
+								<a className="nav-link" href="/wishcards">
+									Wish Cards
+								</a>
+							</div>
+							<div className="nav-item">
+								<a className="nav-link" href="/mission">
+									Mission
+								</a>
+							</div>
+							<div className="nav-item">
+								<a className="nav-link" href="/howto">
+									How To
+								</a>
+							</div>
+							<div className="nav-item">
+								<a className="nav-link" href="/community">
+									Community
+								</a>
+							</div>
+							{!user ? (
+								<div className="nav-item">
+									<a href="/login" className="nav-link">
+										Log In
+									</a>
+								</div>
+							) : user.userRole == 'partner' && agency && agency.isVerified ? (
+								<>
+									<div className="nav-item">
+										<a className="nav-link" href="/wishcards/me">
+											My Wishes
+										</a>
+									</div>
+									<div className="nav-item">
+										<a className="nav-link" href="/wishcards/create">
+											Create a Wish Card
+										</a>
+									</div>
+								</>
+							) : user.userRole == 'admin' ? (
+								<li className="nav-item">
+									<a className="nav-link" href="/admin/">
+										Admin Panel
+									</a>
+								</li>
+							) : (
+								<li className="nav-item">
+									<a className="nav-link" href="/profile">
+										Profile
+									</a>
+								</li>
+							)}
+						</div>
+					</div>
+					<div className="justify-content-end d-lg-none">
+						<button
+							className="navbar-toggler"
+							type="button"
+							data-bs-toggle="collapse"
+							data-bs-target="#navbarToggleExternalContent"
+							aria-controls="navbarToggleExternalContent"
+							aria-expanded="false"
+							aria-label="Toggle navigation"
+						>
+							<i className="fas fa-bars" />
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className="row justify-content-center d-lg-none bg-white">
+				<div className="text-center">
+					<div id="navbarToggleExternalContent" className="collapse">
+						<ul className="navbar-nav">
+							<li className="nav-item">
+								<a href="/wishcards" className="nav-link">
+									Wish Cards
+								</a>
+							</li>
+							<li className="nav-item">
+								<a href="/Mission" className="nav-link">
+									Mission
+								</a>
+							</li>
+							<li className="nav-item">
+								<a href="/howto" className="nav-link">
+									How To
+								</a>
+							</li>
+							<li className="nav-item">
+								<a href="/community" className="nav-link">
+									Community
+								</a>
+							</li>
+							{!user ? (
+								<div className="nav-item">
+									<a href="/login" className="nav-link">
+										Log In
+									</a>
+								</div>
+							) : user?.userRole == 'partner' && agency && agency?.isVerified ? (
+								<>
+									<div className="nav-item">
+										<a className="nav-link" href="/wishcards/me">
+											My Wishes
+										</a>
+									</div>
+									<div className="nav-item">
+										<a className="nav-link" href="/wishcards/create">
+											Create a Wish Card
+										</a>
+									</div>
+								</>
+							) : user?.userRole == 'admin' ? (
+								<li className="nav-item">
+									<a className="nav-link" href="/admin/">
+										Admin Panel
+									</a>
+								</li>
+							) : (
+								<li className="nav-item">
+									<a className="nav-link" href="/profile">
+										Profile
+									</a>
+								</li>
+							)}
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+Nav.propTypes = {
+	user: PropTypes.object,
+	agency: PropTypes.object,
+};
+export default Nav;
