05fe6cfa855475e638430ca58b96661572db6a9a
Enubia <44695100+Enubia@users.noreply.github.com>
2023-08-18 19:29:25 +0200

    feat: added kysely (#620)
    * feat: added kysely
    
    * update: meta_data db field
    
    * Update scripts/init.sql
    
    Co-authored-by: nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
    
    * fix: update script according to reviews
    
    * update: generated types
    
    * update: on delete changes
    
    * update: init script
    
    * update: init script
    
    * update: column types of init script
    
    ---------
    
    Co-authored-by: nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,289 +1,228 @@
-SET check_function_bodies = false;
+CREATE TABLE "agencies" (
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+    "name" varchar(255) NOT NULL,
+    "website" varchar(255) NOT NULL,
+    "phone" varchar(100) NOT NULL,
+    "bio" text,
+    "address_line_1" varchar(255) NOT NULL,
+    "address_line_2" varchar(255),
+    "city" varchar(100) NOT NULL,
+    "state" varchar(100) NOT NULL,
+    "country" varchar(100) NOT NULL,
+    "zip_code" varchar(50) NOT NULL,
+    "verified" boolean NOT NULL DEFAULT false,
+    "employer_identification_number" varchar(100) UNIQUE,
+    "account_manager_id" uuid NOT NULL,
+    "image_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    "updated_at" timestamp
+);
 
-CREATE TABLE
-    IF NOT EXISTS agencies(
-        id integer NOT NULL,
-        "name" varchar(255) NOT NULL,
-        website varchar(255) NOT NULL,
-        phone varchar(100) NOT NULL,
-        bio varchar(500),
-        address_line_1 varchar(255) NOT NULL,
-        address_line_2 varchar(255),
-        city varchar(100) NOT NULL,
-        state varchar(100) NOT NULL,
-        country varchar(100) NOT NULL,
-        zip_code varchar(50) NOT NULL,
-        verified boolean NOT NULL DEFAULT false,
-        employer_identification_number varchar(255) NOT NULL,
-        account_manager_id integer NOT NULL,
-        image_id integer NOT NULL,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at timestamp,
-        CONSTRAINT agencies_pkey PRIMARY KEY(id)
-    );
+CREATE TABLE "children" (
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+    "first_name" varchar(255) NOT NULL,
+    "last_name" varchar(255) NOT NULL,
+    "birth_year" integer NOT NULL,
+    "interest" text NOT NULL,
+    "story" text NOT NULL,
+    "image_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
 
-CREATE TABLE
-    IF NOT EXISTS children(
-        id integer NOT NULL,
-        first_name varchar(255) NOT NULL,
-        last_name varchar(255) NOT NULL,
-        birth_year integer NOT NULL,
-        interest varchar(255) NOT NULL,
-        story varchar(255) NOT NULL,
-        image_id integer NOT NULL,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        CONSTRAINT children_pkey PRIMARY KEY(id)
-    );
+CREATE TABLE "community_posts" (
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+    "message" text NOT NULL,
+    "agency_id" uuid NOT NULL,
+    "image_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
 
-CREATE TABLE
-    IF NOT EXISTS community_posts(
-        id integer NOT NULL,
-        message varchar(255) NOT NULL,
-        agency_id integer NOT NULL,
-        image_id integer NOT NULL,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        CONSTRAINT community_posts_pkey PRIMARY KEY(id)
-    );
+CREATE TABLE "images" (
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+    "url" varchar(255) NOT NULL,
+    "meta_data" json,
+    "created_by" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
 
-CREATE TABLE
-    IF NOT EXISTS images(
-        id integer NOT NULL,
-        url varchar(255) NOT NULL,
-        meta_data json,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        CONSTRAINT images_pkey PRIMARY KEY(id)
-    );
+CREATE TABLE "items" (
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+    "name" varchar(255) NOT NULL,
+    "price" numeric(10, 2) NOT NULL,
+    "link" varchar(2000) NOT NULL,
+    "retailer" varchar(255) NOT NULL,
+    "retailer_product_id" varchar(255) NOT NULL,
+    "meta_data" json,
+    "image_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
 
-CREATE TABLE
-    IF NOT EXISTS items(
-        id integer NOT NULL,
-        "name" varchar(255) NOT NULL,
-        price numeric(10, 2) NOT NULL,
-        link varchar(2000) NOT NULL,
-        retailer varchar(255) NOT NULL,
-        product_id varchar(255) NOT NULL,
-        image_id integer NOT NULL,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        CONSTRAINT items_pkey PRIMARY KEY(id)
-    );
+CREATE TABLE "messages" (
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+    "content" text NOT NULL,
+    "sender_id" uuid NOT NULL,
+    "wishcard_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
 
-CREATE TABLE
-    IF NOT EXISTS messages(
-        id integer NOT NULL,
-        "content" varchar(500) NOT NULL,
-        sender integer NOT NULL,
-        wishcard_id integer NOT NULL,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        CONSTRAINT messages_pkey PRIMARY KEY(id)
-    );
+CREATE TABLE "orders" (
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+    "status" integer NOT NULL DEFAULT 0,
+    "delivery_date" timestamp,
+    "tracking_info" varchar(500),
+    "donor_id" uuid NOT NULL,
+    "child_id" uuid NOT NULL,
+    "item_id" uuid NOT NULL,
+    "agency_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    "updated_at" timestamp
+);
 
-CREATE TABLE
-    IF NOT EXISTS orders(
-        id integer NOT NULL,
-        status integer NOT NULL DEFAULT 0,
-        delivery_date timestamp,
-        tracking_info varchar(500),
-        donor_id integer NOT NULL,
-        child_id integer NOT NULL,
-        item_id integer NOT NULL,
-        agency_id integer NOT NULL,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at timestamp,
-        CONSTRAINT orders_pkey PRIMARY KEY(id)
-    );
+CREATE TABLE "users" (
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+    "first_name" varchar(255) NOT NULL,
+    "last_name" varchar(255) NOT NULL,
+    "email" varchar(255) NOT NULL,
+    "password" varchar(100) NOT NULL,
+    "role" integer NOT NULL,
+    "login_mode" integer NOT NULL,
+    "bio" varchar(500),
+    "is_verified" boolean NOT NULL DEFAULT false,
+    "image_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    "updated_at" timestamp,
+    "deleted_at" timestamp
+);
 
-CREATE TABLE
-    IF NOT EXISTS users(
-        id integer NOT NULL,
-        first_name varchar(255) NOT NULL,
-        last_name varchar(255) NOT NULL,
-        email varchar(255) NOT NULL,
-        "password" varchar(100) NOT NULL,
-        "role" integer NOT NULL,
-        login_mode integer NOT NULL,
-        bio varchar(500),
-        verified boolean NOT NULL DEFAULT false,
-        image_id integer NOT NULL,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at timestamp,
-        deleted_at timestamp,
-        CONSTRAINT users_pkey PRIMARY KEY(id)
-    );
+CREATE TABLE "verification_tokens" (
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+    "token" varchar(255) NOT NULL,
+    "type" integer NOT NULL,
+    "expiration" timestamp NOT NULL,
+    "user_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
 
-CREATE TABLE
-    IF NOT EXISTS verification_tokens(
-        id integer NOT NULL,
-        token varchar(255) NOT NULL,
-        "type" integer NOT NULL,
-        expiration timestamp NOT NULL,
-        user_id integer NOT NULL,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        CONSTRAINT verification_tokens_pkey PRIMARY KEY(id)
-    );
+CREATE TABLE "verifications" (
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+    "email_verified" boolean NOT NULL,
+    "user_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    "updated_at" timestamp
+);
 
-CREATE TABLE
-    IF NOT EXISTS verifications(
-        id integer NOT NULL,
-        email_verified boolean NOT NULL,
-        user_id integer NOT NULL,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at timestamp,
-        CONSTRAINT verifications_pkey PRIMARY KEY(id)
-    );
+CREATE TABLE "wishcards" (
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+    "address_line_1" varchar(255) NOT NULL,
+    "address_line_2" varchar(255),
+    "city" varchar(100) NOT NULL,
+    "state" varchar(100) NOT NULL,
+    "country" varchar(100) NOT NULL,
+    "zip_code" varchar(50) NOT NULL,
+    "status" integer NOT NULL DEFAULT 0,
+    "created_by" uuid NOT NULL,
+    "agency_id" uuid NOT NULL,
+    "child_id" uuid NOT NULL,
+    "item_id" uuid NOT NULL,
+    "image_id" uuid NOT NULL,
+    "order_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    "updated_at" timestamp
+);
 
-CREATE TABLE
-    IF NOT EXISTS wishcards(
-        id integer NOT NULL,
-        address_line_1 varchar(255) NOT NULL,
-        address_line_2 varchar(255),
-        city varchar(100) NOT NULL,
-        state varchar(100) NOT NULL,
-        country varchar(100) NOT NULL,
-        zip_code varchar(50) NOT NULL,
-        status integer NOT NULL DEFAULT 0,
-        created_by integer NOT NULL,
-        agency_id integer NOT NULL,
-        child_id integer NOT NULL,
-        item_id integer NOT NULL,
-        image_id integer NOT NULL,
-        order_id integer NOT NULL,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at timestamp,
-        CONSTRAINT wishcards_pkey PRIMARY KEY(id)
-    );
+ALTER TABLE "verifications" ADD CONSTRAINT "verifications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT;
 
--- constraints
+ALTER TABLE "verification_tokens" ADD CONSTRAINT "verification_tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT;
 
-ALTER TABLE verifications
-ADD
-    CONSTRAINT verifications_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE Cascade;
+ALTER TABLE "agencies" ADD CONSTRAINT "agencies_account_manager_id_fkey" FOREIGN KEY ("account_manager_id") REFERENCES "users" ("id");
 
-ALTER TABLE
-    verification_tokens
-ADD
-    CONSTRAINT verification_tokens_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE Cascade;
+ALTER TABLE "messages" ADD CONSTRAINT "messages_sender_id_fkey" FOREIGN KEY ("sender_id") REFERENCES "users" ("id");
 
-ALTER TABLE agencies
-ADD
-    CONSTRAINT agencies_account_manager_id_fkey FOREIGN KEY (account_manager_id) REFERENCES users (id);
+ALTER TABLE "community_posts" ADD CONSTRAINT "community_posts_agency_id_fkey" FOREIGN KEY ("agency_id") REFERENCES "agencies" ("id") ON DELETE RESTRICT;
 
-ALTER TABLE messages
-ADD
-    CONSTRAINT messages_sender_fkey FOREIGN KEY (sender) REFERENCES users (id);
+ALTER TABLE "wishcards" ADD CONSTRAINT "wishcards_child_id_fkey" FOREIGN KEY ("child_id") REFERENCES "children" ("id");
 
-ALTER TABLE community_posts
-ADD
-    CONSTRAINT community_posts_agency_id_fkey FOREIGN KEY (agency_id) REFERENCES agencies (id) ON DELETE Cascade;
+ALTER TABLE "wishcards" ADD CONSTRAINT "wishcards_agency_id_fkey" FOREIGN KEY ("agency_id") REFERENCES "agencies" ("id") ON DELETE RESTRICT;
 
-ALTER TABLE wishcards
-ADD
-    CONSTRAINT wishcards_child_id_fkey FOREIGN KEY (child_id) REFERENCES children (id);
+ALTER TABLE "wishcards" ADD CONSTRAINT "wishcards_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users" ("id");
 
-ALTER TABLE wishcards
-ADD
-    CONSTRAINT wishcards_agency_id_fkey FOREIGN KEY (agency_id) REFERENCES agencies (id) ON DELETE Cascade;
+ALTER TABLE "wishcards" ADD CONSTRAINT "wishcards_item_id_fkey" FOREIGN KEY ("item_id") REFERENCES "items" ("id");
 
-ALTER TABLE wishcards
-ADD
-    CONSTRAINT wishcards_created_by_fkey FOREIGN KEY (created_by) REFERENCES users (id);
+ALTER TABLE "messages" ADD CONSTRAINT "messages_wishcard_id_fkey" FOREIGN KEY ("wishcard_id") REFERENCES "wishcards" ("id");
 
-ALTER TABLE wishcards
-ADD
-    CONSTRAINT wishcards_item_id_fkey FOREIGN KEY (item_id) REFERENCES items (id);
+ALTER TABLE "wishcards" ADD CONSTRAINT "wishcards_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "images" ("id");
 
-ALTER TABLE messages
-ADD
-    CONSTRAINT messages_wishcard_id_fkey FOREIGN KEY (wishcard_id) REFERENCES wishcards (id);
+ALTER TABLE "children" ADD CONSTRAINT "children_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "images" ("id");
 
-ALTER TABLE wishcards
-ADD
-    CONSTRAINT wishcards_image_id_fkey FOREIGN KEY (image_id) REFERENCES images (id);
+ALTER TABLE "items" ADD CONSTRAINT "items_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "images" ("id");
 
-ALTER TABLE children
-ADD
-    CONSTRAINT children_image_id_fkey FOREIGN KEY (image_id) REFERENCES images (id);
+ALTER TABLE "agencies" ADD CONSTRAINT "agencies_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "images" ("id");
 
-ALTER TABLE items
-ADD
-    CONSTRAINT items_image_id_fkey FOREIGN KEY (image_id) REFERENCES images (id);
+ALTER TABLE "community_posts" ADD CONSTRAINT "community_posts_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "images" ("id");
 
-ALTER TABLE agencies
-ADD
-    CONSTRAINT agencies_image_id_fkey FOREIGN KEY (image_id) REFERENCES images (id);
+ALTER TABLE "users" ADD CONSTRAINT "users_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "images" ("id");
 
-ALTER TABLE community_posts
-ADD
-    CONSTRAINT community_posts_image_id_fkey FOREIGN KEY (image_id) REFERENCES images (id);
+ALTER TABLE "images" ADD CONSTRAINT "images_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users" ("id");
 
-ALTER TABLE users
-ADD
-    CONSTRAINT users_image_id_fkey FOREIGN KEY (image_id) REFERENCES images (id);
+ALTER TABLE "orders" ADD CONSTRAINT "orders_donor_id_fkey" FOREIGN KEY ("donor_id") REFERENCES "users" ("id");
 
-ALTER TABLE orders
-ADD
-    CONSTRAINT orders_donor_id_fkey FOREIGN KEY (donor_id) REFERENCES users (id);
+ALTER TABLE "orders" ADD CONSTRAINT "orders_child_id_fkey" FOREIGN KEY ("child_id") REFERENCES "children" ("id");
 
-ALTER TABLE orders
-ADD
-    CONSTRAINT orders_child_id_fkey FOREIGN KEY (child_id) REFERENCES children (id);
+ALTER TABLE "orders" ADD CONSTRAINT "orders_item_id_fkey" FOREIGN KEY ("item_id") REFERENCES "items" ("id");
 
-ALTER TABLE orders
-ADD
-    CONSTRAINT orders_item_id_fkey FOREIGN KEY (item_id) REFERENCES items (id);
+ALTER TABLE "orders" ADD CONSTRAINT "orders_agency_id_fkey" FOREIGN KEY ("agency_id") REFERENCES "agencies" ("id");
 
-ALTER TABLE orders
-ADD
-    CONSTRAINT orders_agency_id_fkey FOREIGN KEY (agency_id) REFERENCES agencies (id);
+ALTER TABLE "wishcards" ADD CONSTRAINT "wishcards_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
 
-ALTER TABLE wishcards
-ADD
-    CONSTRAINT wishcards_order_id_fkey FOREIGN KEY (order_id) REFERENCES orders (id);
-
--- functions
-CREATE OR REPLACE FUNCTION public.TRIGGER_SET_UPDATED_DATE()
-    RETURNS TRIGGER
-    LANGUAGE PLPGSQL
+CREATE OR REPLACE FUNCTION TRIGGER_SET_UPDATED_DATE()
+    RETURNS trigger
+    LANGUAGE plpgsql
 AS $function$
-	BEGIN
-		NEW.updated_at = NOW();
-		RETURN NEW;
-	END;
-$function$;
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$function$
+;
 
--- triggers
 
-CREATE OR REPLACE TRIGGER SET_UPDATED_AT
+CREATE TRIGGER SET_UPDATED_AT
     before update
-    on public.verifications
+    on verifications
     for each row
-	execute procedure public.TRIGGER_SET_UPDATED_DATE();
-    
-CREATE OR REPLACE TRIGGER SET_UPDATED_AT
-    before update
-    on public.messages
-    for each row
-	execute procedure public.TRIGGER_SET_UPDATED_DATE();
+	execute procedure TRIGGER_SET_UPDATED_DATE();
 
-CREATE OR REPLACE TRIGGER SET_UPDATED_AT
-    before update
-    on public.agencies
-    for each row
-	execute procedure public.TRIGGER_SET_UPDATED_DATE();
 
-CREATE OR REPLACE TRIGGER SET_UPDATED_AT
+CREATE TRIGGER SET_UPDATED_AT
     before update
-    on public.users
+    on agencies
     for each row
-	execute procedure public.TRIGGER_SET_UPDATED_DATE();
-    
-CREATE OR REPLACE TRIGGER SET_UPDATED_AT
-    before update
-    on public.wishcards
-    for each row
-	execute procedure public.TRIGGER_SET_UPDATED_DATE();
+	execute procedure TRIGGER_SET_UPDATED_DATE();
 
-CREATE OR REPLACE TRIGGER SET_UPDATED_AT
+
+CREATE TRIGGER SET_UPDATED_AT
     before update
-    on public.orders
+    on messages
     for each row
-	execute procedure public.TRIGGER_SET_UPDATED_DATE();
+	execute procedure TRIGGER_SET_UPDATED_DATE();
+
+
+CREATE TRIGGER SET_UPDATED_AT
+    before update
+    on orders
+    for each row
+	execute procedure TRIGGER_SET_UPDATED_DATE();
+
+
+CREATE TRIGGER SET_UPDATED_AT
+    before update
+    on users
+    for each row
+	execute procedure TRIGGER_SET_UPDATED_DATE();
+
+
+CREATE TRIGGER SET_UPDATED_AT
+    before update
+    on wishcards
+    for each row
+	execute procedure TRIGGER_SET_UPDATED_DATE();
---
+++ src/db/postgresconnection.ts
@@ -0,0 +1,24 @@
+import { Kysely, KyselyConfig, PostgresDialect } from 'kysely';
+import { Pool } from 'pg';
+
+import { DB } from '../../types/generated/database';
+import config from '../helper/config';
+import logger from '../helper/logger';
+
+const databaseConfig: KyselyConfig = {
+	log(event): void {
+		if (event.level === 'query') {
+			logger.debug(event.query.sql);
+			logger.debug(event.query.parameters);
+		} else if (event.level === 'error') {
+			logger.error(event);
+		}
+	},
+	dialect: new PostgresDialect({
+		pool: new Pool({
+			connectionString: config.DATABASE_URL,
+		}),
+	}),
+};
+
+export const db = new Kysely<DB>(databaseConfig);
