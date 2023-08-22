da003ae32f12b84db7a5d393f5738ec81d711742
Stacy Lee <stacy.lee@hhs.texas.gov>
2023-08-22 10:30:35 -0500

    removed the constants, will make a separate branch and separate PR per request

--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const USER_ROLES_MAP = { 0: 'donor', 1: 'partner', 2: 'admin' };
-export const LOGIN_MODES_MAP = { 0: 'email', 1: 'facebook', 2: 'google' };
-export const ORDER_STATUS_MAP = { 0: 'pending', 1: 'ordered', 2: 'delivered', 3: 'cancelled' };
-export const WISHCARD_STATUS_MAP = { 0: 'draft', 1: 'published', 2: 'donated' };
-export const VERIFICATION_TYPES_MAP = { 0: 'email', 1: 'phone' };
