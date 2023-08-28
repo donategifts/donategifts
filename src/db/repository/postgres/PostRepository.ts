c96e644f9bec52cf1b5ec0073eb7eec97dac5b1e
Enubia <44695100+Enubia@users.noreply.github.com>
2023-08-28 14:23:38 +0200

    refactor: post repository as example

---
+++ src/db/repository/postgres/PostRepository.ts
@@ -0,0 +1,28 @@
+import { Kysely } from 'kysely';
+
+import { DB, CommunityPosts } from '../../types/generated/database';
+
+export default class PostRepository {
+	constructor(private readonly database: Kysely<DB>) {}
+
+	async getAllPosts() {
+		return await this.database
+			.selectFrom('community_posts')
+			.innerJoin('agencies', 'community_posts.id', 'agencies.id')
+			.selectAll()
+			.execute();
+	}
+
+	async getAllPostsByVerifiedAgencies() {
+		return await this.database
+			.selectFrom('community_posts')
+			.innerJoin('agencies', 'community_posts.id', 'agencies.id')
+			.where('agencies.verified', '=', true)
+			.selectAll()
+			.execute();
+	}
+
+	async createNewPost(postParams: Omit<CommunityPosts, 'id' | 'created_at'>) {
+		return await this.database.insertInto('community_posts').values(postParams).execute();
+	}
+}
