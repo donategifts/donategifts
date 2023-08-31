05bdc1484c4a7a0297de6c65fd0ae031ccd165e3
Enubia <44695100+Enubia@users.noreply.github.com>
2023-08-31 13:12:56 +0200

    chore: naimg

--- src/db/repository/postgres/MessagesRepository.ts
+++ src/db/repository/postgres/MessagesRepository.ts
@@ -5,15 +5,15 @@ import { DB, Messages } from '../../types/generated/database';
 export default class MessagesRepository {
 	constructor(private readonly database: Kysely<DB>) {}
 
-	getMessageByObjectId(messageId: string) {
-		return this.database.selectFrom('messages').where('id', '=', messageId).execute();
+	getMessageById(id: string) {
+		return this.database.selectFrom('messages').where('id', '=', id).execute();
 	}
 
 	async createNewMessage(messageParams: Omit<Messages, 'id' | 'created_at'>) {
 		await this.database.insertInto('messages').values(messageParams).execute();
 	}
 
-	getMessagesByWishCardId(wishcardId: string) {
-		return this.database.selectFrom('messages').where('wishcard_id', '=', wishcardId).execute();
+	getMessagesByWishCardId(id: string) {
+		return this.database.selectFrom('messages').where('wishcard_id', '=', id).execute();
 	}
 }
