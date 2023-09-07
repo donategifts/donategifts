b49872d0b903caad6615f0f07f414ca7c0ca7476
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2023-09-06 21:36:11 -0400

    added import and save seeder file helper functions

--- scripts/prepareDbExport.js
+++ scripts/prepareDbExport.js
@@ -10,10 +10,40 @@ const randomNumber = (min = 0, max = 100) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
 };
 
+const importSeederFile = async (name = '') => {
+    const filePath = path.join(__dirname, `./seeder-data/${name}.json`);
+    
+    try {
+        const fileData = await fs.promises.readFile(filePath, 'utf8');
+        const JSONData = JSON.parse(fileData);
+        
+        return JSONData;
+    } catch (error) {
+        console.log('Error importing seeder file', { filePath, error });
+    }
+    
+    return;
+};
+
+const saveSeederFile = async (name = '', data = []) => {
+    const filePath = path.join(__dirname, `./seeder-data/${name}.json`);
+    
+    try {
+        const fileData = JSON.stringify(data, null, 4);
+        await fs.promises.writeFile(filePath, fileData, 'utf8');
+        
+        return;
+    } catch (error) {
+        console.log('Error saving seeder file', { filePath, error });
+    }
+    
+    return;
+};
+
 (async () => {
 	try {
-		const prepareAgencies = () => {
-			const agencies = require('./seeder-data/agencies.json');
+		const prepareAgencies = async () => {
+			const agencies = await importSeederFile('agencies');
 			const agenciesData = agencies.map((agency) => {
 				const {
                     id = randomUUID(),
