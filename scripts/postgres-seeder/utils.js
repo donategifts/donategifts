19152c8fcb00d42c2e20e18cbeaae18dd9a6f82c
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2023-09-13 01:49:53 -0400

    added missing semicolon

--- scripts/postgres-seeder/utils.js
+++ scripts/postgres-seeder/utils.js
@@ -32,7 +32,7 @@ const saveSeederFile = async (name = '', data = []) => {
 };
 
 const randomNumber = (min = 0, max = 100) => {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 module.exports = {
