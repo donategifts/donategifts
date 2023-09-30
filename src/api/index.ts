e9939bf3cca5ce554107279bda17a50645eae779
Enubia <44695100+Enubia@users.noreply.github.com>
2023-09-30 20:08:34 +0200

    new: added verification link resend option (#683)

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import agency from './agency';
 import community from './community';
+import profile from './profile';
 import wishcards from './wishcards';
 
 export const routes = Router();
@@ -9,3 +10,4 @@ export const routes = Router();
 routes.use('/agency', agency);
 routes.use('/community', community);
 routes.use('/wishcards', wishcards);
+routes.use('/profile', profile);
