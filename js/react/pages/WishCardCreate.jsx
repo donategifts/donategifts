0d61d09ef9d71549cf91cf6ad25c1ac0bbd28385
Stacy Lee <stacy.lee@hhs.texas.gov>
2023-10-03 02:51:19 -0500

    added more fields for the 1st section in wishcard create form

--- js/react/pages/WishCardCreate.jsx
+++ js/react/pages/WishCardCreate.jsx
@@ -1,45 +1,94 @@
 function WishCardCreate() {
 	return (
-		<div>
+		<div className="wish-create-page">
 			<div className="container">
 				<h1 className="heading-primary my-5 text-center">Create a wish card</h1>
-				<div className="card shadow-lg">
-					<div className="card-body d-flex flex-column p-5">
-						<div className="row justify-content-center">
-							<div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-								<label
-									htmlFor="childFirstName"
-									className="text-primary text-center"
-								>
-									First name of the child:{' '}
-								</label>
+				<div className="card shadow-lg p-4">
+					<div className="card-body">
+						<form action="">
+							<div className="row py-3">
+								<div className="form-group col-md-6 mb-3 mb-md-0">
+									<label htmlFor="childFirstName" className="form-label">
+										Child&apos;s first name
+									</label>
+									<input
+										type="text"
+										name="childFirstName"
+										id="childFirstName"
+										className="form-control"
+									/>
+								</div>
+								<div className="form-group col-md-6">
+									<label htmlFor="childInterest" className="form-label">
+										Child&apos;s interest
+									</label>
+									<input
+										type="text"
+										name="childInterest"
+										id="childInterest"
+										placeholder="write something they like to do"
+										className="form-control"
+									/>
+								</div>
 							</div>
-							<div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-								<input
-									type="text"
-									name="childFirstName"
-									id="childFirstName"
-									className="input-border-secondary w-75"
-								/>
+							<div className="row py-3">
+								<div className="form-group col-md-6 mb-3 mb-md-0">
+									<label htmlFor="wishItemName" className="form-label">
+										Wish item name
+									</label>
+									<input
+										type="text"
+										name="wishItemName"
+										id="wishItemName"
+										className="form-control"
+									/>
+								</div>
+								<div className="form-group col-md-6">
+									<label htmlFor="wishItemPrice" className="form-label">
+										Wish item price
+										<i className="fa fa-question-circle ms-1"></i>
+									</label>
+									<input
+										type="number"
+										name="wishItemPrice"
+										id="wishItemPrice"
+										placeholder="price must be under $40"
+										className="form-control"
+										min={1}
+										max={40}
+									/>
+								</div>
 							</div>
-						</div>
-						<div className="row justify-content-center">
-							<div className="col">
-								<p>Child&apos;s Interests</p>
+							<div className="row py-3">
+								<div className="col-12">
+									<label htmlFor="wishItemDesc" className="form-label">
+										Wish item description
+										<i className="fa fa-question-circle ms-1"></i>
+									</label>
+									<input
+										type="text"
+										name="wishItemDesc"
+										id="wishItemDesc"
+										className="form-control"
+										placeholder="share product details"
+									/>
+								</div>
 							</div>
-							<div className="col">
-								<p>
-									<i className="fas fa-question-circle"></i>click for help
-								</p>
+							<div className="row py-3">
+								<div className="col-12">
+									<label htmlFor="wishItemURL" className="form-label">
+										Wish item Amazon URL
+									</label>
+									<input
+										type="text"
+										name=""
+										id=""
+										className="form-control"
+										placeholder="product page link starting with https://www.amazon..."
+									/>
+								</div>
 							</div>
-						</div>
-						<textarea
-							name="childInterest"
-							id="childInterest"
-							cols="30"
-							rows="10"
-							placeholder="write something they like to do"
-						></textarea>
+						</form>
 					</div>
 				</div>
 			</div>
