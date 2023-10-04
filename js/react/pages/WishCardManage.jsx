b7f15469f5baebb860aa06400b16c8ccd4df939c
Stacy Lee <stacy.lee@hhs.texas.gov>
2023-10-04 04:02:16 -0500

    #645 wish card manage page redesign

--- js/react/pages/WishCardManage.jsx
+++ js/react/pages/WishCardManage.jsx
@@ -1,3 +1,4 @@
+import { Tabs } from '@mantine/core';
 import axios from 'axios';
 import { useState, useEffect, useRef } from 'react';
 
@@ -7,60 +8,65 @@ import MantineProviderWrapper from '../utils/mantineProviderWrapper.jsx';
 
 const renderAgencyWishCards = ({ wishCards, emptyMessage, onClickEditWishcard }) => {
 	return !wishCards || wishCards.length === 0 ? (
-		<h6 className="cool-font">{emptyMessage}</h6>
+		<div className="mt-3 p-5 d-flex justify-content-center flex-column align-items-center">
+			<h6 className="text-center mb-3">{emptyMessage}</h6>
+			<div className="margin-auto">
+				<a href="/wishcards/create" className="button-accent">
+					Create Wish Cards
+				</a>
+			</div>
+		</div>
 	) : (
 		<div className="row justify-content-center">
 			{wishCards.map((card) => {
-				const title = 'My name is ' + card.childFirstName;
-				const status = 'Status: ' + card.status;
-				const wishItemName = 'Wish: ' + card.wishItemName;
-				const price = 'Price: $' + card.wishItemPrice;
+				const title = 'Name: ' + card.childFirstName;
+				const status = 'Wish Card Status: ' + card.status;
+				const wishItemName = 'Wish Item: ' + card.wishItemName;
+				const price = 'Item Price: $' + card.wishItemPrice;
 				const interest = 'Interest: ' + card.childInterest;
 
 				const handleClickEditIcon = () => {
 					onClickEditWishcard(card);
 				};
+
 				return (
-					<div className="col-lg-4 col-xs-12 mb-5 mt-4" key={card._id}>
-						<div className="card mb-3">
+					<div id="wishcards" className="col-lg-4 col-xs-12 mb-5 mt-4" key={card._id}>
+						<div className="card border-0 shadow h-100 mb-1">
 							<div className="view overlay">
 								<img
 									src={card.wishCardImage}
 									alt="Card image"
 									id="img-fix"
-									className="card-img-top"
+									className="card-img-top rounded-0 rounded-top-3"
 								/>
 								<a href="#"></a>
 								<div className="mask rgba-white-slight"></div>
 								<div
 									onClick={handleClickEditIcon}
 									className="btn btn-secondary fas fa-edit pointer position-absolute top-0 end-0 mt-2 me-2"
-									// data-bs-toggle="modal"
-									// data-bs-target="#wishcard-edit-modal"
 								></div>
 							</div>
-
-							<div className="card-body">
+							<div className="card-body rounded-0 rounded-bottom-3">
 								<div className="card-text-container">
-									<h3 className="card-title text-center">{title}</h3>
-									<div className="quick-font mb-3">
+									<p className="card-title">{title}</p>
+									<div className="quick-font mb-1">
 										<p className="card-text">
 											<span className="font-weight-bold">{status}</span>
 										</p>
 									</div>
-									<div className="quick-font mb-3">
+									<div className="quick-font mb-1">
 										<p className="card-text">
 											<span className="font-weight-bold truncate-ellipsis">
 												{wishItemName}
 											</span>
 										</p>
 									</div>
-									<div className="quick-font mb-3">
+									<div className="quick-font mb-1">
 										<p className="card-text">
 											<span className="font-weight-bold">{price}</span>
 										</p>
 									</div>
-									<div className="quick-font mb-3">
+									<div className="quick-font mb-1">
 										<p className="card-text">
 											<span className="font-weight-bold truncate-ellipsis">
 												{interest}
@@ -133,32 +139,38 @@ export default function WishCardManage() {
 	return (
 		<MantineProviderWrapper>
 			<div className="wishcards">
-				<div className="margin-auto d-flex justify-content-center">
-					<a href="/wishcards/create" className="button-accent">
-						Create More Wish Cards
-					</a>
-				</div>
-				<div className="container">
-					<h3 className="cool-font">Draft Wishcards</h3>
-					{renderAgencyWishCards({
-						emptyMessage: 'No draft wishcards',
-						wishCards: agencyCards.draftWishcards,
-						onClickEditWishcard: handleClickEditOnCard,
-					})}
+				<div className="container pt-3">
+					<Tabs color="#ff826b" radius="md" defaultValue="draft">
+						<Tabs.List>
+							<Tabs.Tab value="draft">Draft</Tabs.Tab>
+							<Tabs.Tab value="published">Published</Tabs.Tab>
+							<Tabs.Tab value="donated">Donated</Tabs.Tab>
+						</Tabs.List>
 
-					<h3 className="cool-font">Active Wishcards</h3>
-					{renderAgencyWishCards({
-						emptyMessage: 'You have 0 active wishcards',
-						wishCards: agencyCards.activeWishcards,
-						onClickEditWishcard: handleClickEditOnCard,
-					})}
+						<Tabs.Panel value="draft">
+							{renderAgencyWishCards({
+								emptyMessage: 'You have 0 draft wish cards.',
+								wishCards: agencyCards.draftWishcards,
+								onClickEditWishcard: handleClickEditOnCard,
+							})}
+						</Tabs.Panel>
 
-					<h3 className="cool-font">Inactive Wishcards</h3>
-					{renderAgencyWishCards({
-						emptyMessage: 'You have 0 inactive wishcards',
-						wishCards: agencyCards.inactiveWishcards,
-						onClickEditWishcard: handleClickEditOnCard,
-					})}
+						<Tabs.Panel value="published">
+							{renderAgencyWishCards({
+								emptyMessage: 'You have 0 published wish cards.',
+								wishCards: agencyCards.activeWishcards,
+								onClickEditWishcard: handleClickEditOnCard,
+							})}
+						</Tabs.Panel>
+
+						<Tabs.Panel value="donated">
+							{renderAgencyWishCards({
+								emptyMessage: 'You have 0 donated wish cards.',
+								wishCards: agencyCards.inactiveWishcards,
+								onClickEditWishcard: handleClickEditOnCard,
+							})}
+						</Tabs.Panel>
+					</Tabs>
 				</div>
 				<SimpleModal
 					// modalId="wishcard-edit-modal"
