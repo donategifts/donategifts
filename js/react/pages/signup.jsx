8091d813d08b4ba5e0827d25818770e75e555c2b
Amadou Traore <amadou.f_traore@hotmail.com>
2023-11-30 06:44:53 -0500

    Convert signup to react (#781)

---
+++ js/react/pages/signup.jsx
@@ -0,0 +1,213 @@
+import axios from 'axios';
+import { useState, useRef } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
+
+function Signup() {
+	const [formState, setFormState] = useState({
+		fName: '',
+		lName: '',
+		email: '',
+		password: '',
+		passwordConfirm: '',
+	});
+	const [userRole, setUserRole] = useState('donor');
+	const [showPassword, setShowPassword] = useState(false);
+	const recaptchaRef = useRef();
+
+	function handleChange(e) {
+		setFormState((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}));
+	}
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		const captchaToken = await recaptchaRef.current.executeAsync();
+		recaptchaRef.current.reset();
+		axios
+			.post('/signup', {
+				...formState,
+				userRole,
+				captchaToken,
+			})
+			.then((res) => {
+				location.assign(res.data.url);
+			})
+			.catch((err) => {
+				new window.DG.Toast().show(
+					err.response.data.error.msg || err.response.data.error,
+					window.DG.Toast.styleMap.danger,
+				);
+			});
+	}
+
+	return (
+		<div
+			className="gradient-form d-flex justify-content-center align-items-center flex-column"
+			id="sign-up"
+		>
+			<h1 className="heading-primary mt-5">Join DonateGifts!</h1>
+			<div className="container pt-4 pb-5">
+				<form className="p-5 rounded-4" id="sign-up-form" onSubmit={handleSubmit}>
+					<div className="col-md-8 mx-auto text-white">
+						<div className="py-2">
+							<label>I&#39;m signing up as a:</label>
+							<div className="row px-2">
+								<input
+									className="btn-check col-auto"
+									id="donor"
+									type="radio"
+									name="user-role"
+									value="donor"
+									autoComplete="off"
+									checked={userRole === 'donor'}
+									onChange={(e) => setUserRole(e.target.value)}
+								/>
+								<label
+									className="fw-bold btn btn-outline-light col m-1 py-3 d-flex flex-column justify-content-center"
+									htmlFor="donor"
+								>
+									Gift Sender
+								</label>
+								<input
+									className="btn-check col-auto"
+									id="partner"
+									type="radio"
+									name="user-role"
+									value="partner"
+									autoComplete="off"
+									data-bs-toggle="modal"
+									data-bs-target="#signupModalCenter"
+									checked={userRole === 'partner'}
+									onChange={(e) => setUserRole(e.target.value)}
+								/>
+								<label
+									className="fw-bold btn btn-outline-light col m-1 py-3 d-flex flex-column justify-content-center"
+									htmlFor="partner"
+								>
+									Nonprofit Agency Partner
+								</label>
+							</div>
+						</div>
+						<div className="py-2">
+							<label className="form-label" htmlFor="fName">
+								First Name:
+							</label>
+							<input
+								className="form-control bg-transparent border-0 rounded-0 border-bottom border-white"
+								id="fName"
+								required="required"
+								type="text"
+								name="fName"
+								value={formState.fName}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="py-2">
+							<label className="form-label" htmlFor="lName">
+								Last Name:
+							</label>
+							<input
+								className="form-control bg-transparent border-0 rounded-0 border-bottom border-white"
+								id="lName"
+								required="required"
+								type="text"
+								name="lName"
+								value={formState.lName}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="py-2">
+							<label className="form-label" htmlFor="email">
+								Email:
+							</label>
+							<input
+								className="form-control bg-transparent border-0 rounded-0 border-bottom border-white"
+								id="email"
+								required="required"
+								type="email"
+								name="email"
+								value={formState.email}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="py-2">
+							<label className="form-label" htmlFor="password">
+								Password:
+							</label>
+							<div>
+								<div className="input-group">
+									<input
+										className="form-control bg-transparent border-0 rounded-0 border-bottom border-white"
+										id="password"
+										required="required"
+										type={showPassword ? 'text' : 'password'}
+										name="password"
+										value={formState.password}
+										onChange={handleChange}
+									/>
+									<span className="input-group-text bg-transparent border-0 rounded-0 border-bottom border-white">
+										<div
+											className={
+												(showPassword ? 'fas fa-eye-slash' : 'fas fa-eye') +
+												' text-white pointer'
+											}
+											id="showPassword"
+											onClick={() => setShowPassword(!showPassword)}
+										/>
+									</span>
+								</div>
+								{formState.passwordConfirm !== '' &&
+									formState.password !== formState.passwordConfirm && (
+										<small className="text-primary" id="password-error">
+											The passwords dont match.
+										</small>
+									)}
+							</div>
+						</div>
+						<div className="py-2">
+							<label className="form-label" htmlFor="passwordConfirm">
+								Confirm Password:
+							</label>
+							<input
+								className="form-control bg-transparent border-0 rounded-0 border-bottom border-white"
+								id="passwordConfirm"
+								required="required"
+								type="password"
+								name="passwordConfirm"
+								value={formState.passwordConfirm}
+								onChange={handleChange}
+							/>
+						</div>
+						<ReCAPTCHA
+							className="g-recaptcha"
+							size="invisible"
+							sitekey="6LcaNtcZAAAAAJ4wxLdiUe4fc1sLhkILRs-DXnXe"
+							ref={recaptchaRef}
+						/>
+						<div className="row pt-4 justify-content-center">
+							<div className="col-md-6">
+								<button
+									className="btn btn-lg btn-primary w-100 d-flex justify-content-center"
+									id="submit-btn"
+									type="submit"
+								>
+									Sign Up
+								</button>
+								{userRole === 'donor' && (
+									<div
+										className="mt-3 center-elements"
+										id="google-login-button"
+									/>
+								)}
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
+
+export default Signup;
