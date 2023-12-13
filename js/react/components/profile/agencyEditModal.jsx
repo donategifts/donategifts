88608b3f809618c55defa8827f5794768ae595ba
Jacobjohnjeevan <mail@jacobjeevan.me>
2023-12-13 10:37:03 -0600

    agencyEditModal - useForm
    - Switched to using useForm from mantine for state management
    - Added validation
    	- Masking for phone numbers
    - Refs to handle scrolling on errors

--- js/react/components/profile/agencyEditModal.jsx
+++ js/react/components/profile/agencyEditModal.jsx
@@ -1,39 +1,12 @@
 import { Button, Container, Modal, TextInput, Textarea } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import PropType from 'prop-types';
-import { useState } from 'react';
+import { useRef } from 'react';
 
 function AgencyEditModal({ agency, opened, onClose, formSubmit }) {
-	const [userAgency, setUserAgency] = useState({
-		agencyBio: agency.agencyBio,
-		agencyPhone: agency.agencyPhone,
-		agencyWebsite: agency.agencyWebsite || '',
-		address1: agency.agencyAddress.address1 || '',
-		address2: agency.agencyAddress.address2 || '',
-		city: agency.agencyAddress.city || '',
-		state: agency.agencyAddress.state || '',
-		country: agency.agencyAddress.country || '',
-		zipcode: agency.agencyAddress.zipcode || '',
-	});
-
-	const handleInputChange = (event) => {
-		const target = event.target;
-		const { name, value } = target;
-
-		setUserAgency({
-			...userAgency,
-			[name]: value,
-		});
-	};
-
-	const getInputProps = (name) => ({
-		name,
-		value: userAgency[name],
-		onChange: handleInputChange,
-	});
-
-	const closeAndReset = () => {
-		setUserAgency({
-			...userAgency,
+	const formRefs = {};
+	const form = useForm({
+		initialValues: {
 			agencyBio: agency.agencyBio,
 			agencyPhone: agency.agencyPhone,
 			agencyWebsite: agency.agencyWebsite || '',
@@ -43,7 +16,75 @@ function AgencyEditModal({ agency, opened, onClose, formSubmit }) {
 			state: agency.agencyAddress.state || '',
 			country: agency.agencyAddress.country || '',
 			zipcode: agency.agencyAddress.zipcode || '',
-		});
+		},
+		validate: {
+			agencyBio: (value) =>
+				value.length < 5 || value.length > 500 ? 'Must be 5 to 500 characters long.' : null,
+			agencyPhone: (value) =>
+				/^\([2-9]\d{2}\) \d{3}-\d{4}$/.test(value)
+					? null
+					: `Phone number must be 10 digits and follow the format of (333) 444-5555 ${value}`,
+			address1: (value) =>
+				value.length < 2 || value.length > 250
+					? 'Address line 1 must contain at least 2 characters.'
+					: null,
+			city: (value) =>
+				value.length < 2 || value.length > 250
+					? 'Address line 1 must contain at least 2 characters.'
+					: null,
+		},
+	});
+
+	const formatPhone = (e) => {
+		const input = e.target.value.replace(/\D/g, '');
+		const length = input.length;
+		let formattedPhone = '';
+
+		if (e.nativeEvent.inputType === 'deleteContentBackward') {
+			formattedPhone = input;
+		} else if (length > 3) {
+			// Formatting as "(123) 456-7890"
+			formattedPhone = `(${input.slice(0, 3)}) ${input.slice(3, 6)}-${input.slice(6, 10)}`;
+		} else {
+			// For inputs like "123" or "12" or "1" or "1-"
+			formattedPhone = input;
+		}
+
+		return formattedPhone;
+	};
+
+	const handleInputChangePhone = (event) => {
+		form.setValues({ agencyPhone: formatPhone(event) });
+	};
+
+	const getInputProps = (name) => {
+		const currentRef = useRef();
+		formRefs[`${name}Ref`] = currentRef;
+		return {
+			ref: currentRef,
+			...form.getInputProps(name),
+		};
+	};
+
+	const handleScroll = (ref) => {
+		if (ref?.current) {
+			window?.scrollTo({
+				top: ref.offsetTop,
+				left: 0,
+				behavior: 'smooth',
+			});
+			ref.current.focus();
+		}
+	};
+
+	const handleError = (errors) => {
+		const firstError = Object.keys(errors)[0];
+		const errorRef = formRefs[`${firstError}Ref`];
+		handleScroll(errorRef);
+	};
+
+	const closeAndReset = () => {
+		form.reset();
 		onClose();
 	};
 
@@ -66,8 +107,7 @@ function AgencyEditModal({ agency, opened, onClose, formSubmit }) {
 								rows="3"
 								placeholder="Enter agency description"
 								label="Agency Description:"
-								defaultValue={userAgency.agencyBio}
-								onChange={handleInputChange}
+								{...getInputProps('agencyBio')}
 							/>
 						</div>
 						<div className="my-2">
@@ -79,6 +119,7 @@ function AgencyEditModal({ agency, opened, onClose, formSubmit }) {
 								placeholder="000-000-0000"
 								label="Contact Number:"
 								{...getInputProps('agencyPhone')}
+								onChange={handleInputChangePhone}
 							/>
 						</div>
 						<div className="my-2">
@@ -155,7 +196,7 @@ function AgencyEditModal({ agency, opened, onClose, formSubmit }) {
 							<Button
 								size="lg"
 								className="btn btn-primary w-25"
-								onClick={() => formSubmit(userAgency)}
+								onClick={form.onSubmit((values) => formSubmit(values), handleError)}
 							>
 								Save
 							</Button>
